6b51283f3e057b8d5e3629d0169aa4a1860d9409
Steve <devnull@localhost>
2006-07-15 22:13:03 -0600

    Initial revision

---
+++ prince/prince-prefs.js
@@ -0,0 +1,7 @@
+pref("toolkit.defaultChromeURI", "chrome://prince/content/prince.xul");
+pref("general.useragent.extra.prince", "Prince/0.1");
+
+/* debugging prefs */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
