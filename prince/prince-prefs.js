c1e352e0ec439c65ac9f79a1ca9adfaab24ffffb
MSI\barry <devnull@localhost>
2006-07-25 19:23:49 -0600

    Changes to Venkman to get it to run in Prince2. Minor fixups

--- prince/prince-prefs.js
+++ prince/prince-prefs.js
@@ -5,3 +5,5 @@ pref("general.useragent.extra.prince", "Prince/0.1");
 pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.showInConsole", true);
 pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
