e7a32ef641cec8a966c7d12e973e8ac041b63bd0
barry <devnull@localhost>
2006-07-30 11:50:39 -0600

    Brought over other prefs from old Prince

--- prince/prince-prefs.js
+++ prince/prince-prefs.js
@@ -7,3 +7,139 @@ pref("javascript.options.showInConsole", true);
 pref("javascript.options.strict", true);
 pref("nglayout.debug.disable_xul_cache", true);
 pref("nglayout.debug.disable_xul_fastload", true);
+
+pref("javascript.enabled",                  true);
+
+
+pref("editor.author",                       "");
+
+pref("editor.text_color",                   "#000000");
+pref("editor.link_color",                   "#0000FF");
+pref("editor.active_link_color",            "#000088");
+pref("editor.followed_link_color",          "#FF0000");
+pref("editor.background_color",             "#FFFFFF");
+pref("editor.use_background_image",         false);
+pref("editor.default_background_image",     "");
+pref("editor.use_custom_default_colors", 1);
+
+pref("editor.hrule.height",                 2);
+pref("editor.hrule.width",                  100);
+pref("editor.hrule.width_percent",          true);
+pref("editor.hrule.shading",                true);
+pref("editor.hrule.align",                  1);
+
+pref("editor.table.maintain_structure", true);
+
+pref("editor.prettyprint", true);
+
+pref("editor.throbber.url","chrome://editor-region/locale/region.properties");
+
+pref("editor.toolbars.showbutton.new", true);
+pref("editor.toolbars.showbutton.open", true);
+pref("editor.toolbars.showbutton.save", true);
+pref("editor.toolbars.showbutton.publish", true);
+pref("editor.toolbars.showbutton.preview", true);
+pref("editor.toolbars.showbutton.cut", false);
+pref("editor.toolbars.showbutton.copy", false);
+pref("editor.toolbars.showbutton.paste", false);
+pref("editor.toolbars.showbutton.print", true);
+pref("editor.toolbars.showbutton.find", false);
+pref("editor.toolbars.showbutton.image", true);
+pref("editor.toolbars.showbutton.hline", false);
+pref("editor.toolbars.showbutton.table", true);
+pref("editor.toolbars.showbutton.link", true);
+pref("editor.toolbars.showbutton.namedAnchor", false);
+pref("editor.toolbars.showbutton.form", true);
+
+pref("editor.toolbars.showbutton.bold", true);
+pref("editor.toolbars.showbutton.italic", true);
+pref("editor.toolbars.showbutton.underline", true);
+pref("editor.toolbars.showbutton.DecreaseFontSize", true);
+pref("editor.toolbars.showbutton.IncreaseFontSize", true);
+pref("editor.toolbars.showbutton.ul", true);
+pref("editor.toolbars.showbutton.ol", true);
+pref("editor.toolbars.showbutton.outdent", true);
+pref("editor.toolbars.showbutton.indent", true);
+
+pref("editor.auto_save",                    false);
+pref("editor.auto_save_delay",              10);
+pref("editor.use_html_editor",              0);
+pref("editor.html_editor",                  "");
+pref("editor.use_image_editor",             0);
+pref("editor.image_editor",                 "");
+
+pref("editor.history.url_maximum", 10);
+
+pref("editor.publish.",                      "");
+pref("editor.lastFileLocation.image",        "");
+pref("editor.lastFileLocation.html",         "");
+pref("editor.save_associated_files",         true);
+pref("editor.always_show_publish_dialog",    false);
+
+/*
+ * What are the entities that you want Mozilla to save using mnemonic
+ * names rather than numeric codes? E.g. If set, we'll output &nbsp;
+ * otherwise, we may output 0xa0 depending on the charset.
+ *
+ * "none"   : don't use any entity names; only use numeric codes.
+ * "basic"  : use entity names just for &nbsp; &amp; &lt; &gt; &quot; for 
+ *            interoperability/exchange with products that don't support more
+ *            than that.
+ * "latin1" : use entity names for 8bit accented letters and other special
+ *            symbols between 128 and 255.
+ * "html"   : use entity names for 8bit accented letters, greek letters, and
+ *            other special markup symbols as defined in HTML4.
+ */
+//pref("editor.encode_entity",                 "html");
+
+pref("editor.grid.snap",                     false);
+pref("editor.grid.size",                     0);
+pref("editor.resizing.preserve_ratio",       true);
+pref("editor.positioning.offset",            0);
+
+/* for Prince */
+pref("mousewheel.withcontrolkey.action", 3); //enable ctrl+mousewheel to change font size
+
+pref("fastcursor.enabled", true);
+pref("fastcursor.flashbar", 1);
+pref("fastcursor.tapsforrepeat", 1);
+pref("fastcursor.millisecondsfortaps", 500);
+
+pref("prince.zoom_factor",            "1.0");
+pref("prince.graph.XTickCount", "0");
+pref("prince.graph.YTickCount", "0");
+pref("prince.graph.ZTickCount", "0");
+pref("prince.graph.XAxisLabel", "");
+pref("prince.graph.YAxisLabel", "");
+pref("prince.graph.ZAxisLabel", "");
+pref("prince.graph.Dimension",  "2");
+pref("prince.graph.ZAxisLabel", "");
+pref("prince.graph.DefaultFileType", "png");
+pref("prince.plot.XMax", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>6</mn></mrow></math>"); 
+pref("prince.plot.XMin", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mo form=\"prefix\">-</mo><mn>6</mn></mrow></math>");
+pref("prince.plot.YMax", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>6</mn></mrow></math>");
+pref("prince.plot.YMin", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mo form=\"prefix\">-</mo><mn>6</mn></mrow></math>");
+pref("prince.plot.ZMax", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>6</mn></mrow></math>");
+pref("prince.plot.ZMin", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mo form=\"prefix\">-</mo><mn>6</mn></mrow></math>");
+
+pref("prince.plot.XVar", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mi>x</mi></mrow></math>"); 
+pref("prince.plot.YVar", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mi>y</mi></mrow></math>"); 
+pref("prince.plot.ZVar", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mi>t</mi></mrow></math>"); 
+pref("prince.plot.XPts", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>6</mn></mrow></math>");
+pref("prince.plot.YPts", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>6</mn></mrow></math>");
+pref("prince.plot.ZPts", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>6</mn></mrow></math>");
+
+pref("prince.plot.Animate",        "false");
+pref("prince.plot.DiscAdjust",     "true");
+pref("prince.plot.IncludeLines",   "true");
+pref("prince.plot.TubeRadius",     "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>1</mn></mrow></math>");
+pref("prince.plot.AISubIntervals", "10");
+pref("prince.plot.AIMethod",       "Middle");
+pref("prince.plot.AIInfo",         "NoInfo");
+
+pref("prince.plot.XPts", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>30</mn></mrow></math>");
+pref("prince.plot.YPts", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>20</mn></mrow></math>");
+pref("prince.plot.ZPts", "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mrow><mn>20</mn></mrow></math>");
+
+pref("prince.bibtex.dir", "/swp55/TCITeX/BibTeX/bib");
+pref("security.xpconnect.plugin.unrestricted", "true");
