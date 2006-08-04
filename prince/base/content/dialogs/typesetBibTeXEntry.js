8214b1c2b943b49648a78af623e99c945d86f158
barry <devnull@localhost>
2006-08-04 09:20:47 -0600

    Moved files from prince to prince\\base

---
+++ prince/base/content/dialogs/typesetBibTeXEntry.js
@@ -0,0 +1,62 @@
+// Copyright (c) 2005 MacKichan Software, Inc.  All Rights Reserved.
+
+var gBodyElement;
+
+const emptyElementStr=" ";
+
+//const mmlns    = "http://www.w3.org/1998/Math/MathML";
+const xhtmlns  = "http://www.w3.org/1999/xhtml";
+
+var data;
+
+// dialog initialization code
+function Startup()
+{
+  var editor = GetCurrentEditor();
+  if (!editor) {
+    window.close();
+    return;
+  }
+
+  doSetOKCancel(onAccept, onAccept);
+  data = window.arguments[0];
+  data.Cancel = false;
+  gDialog.theData = data.theData;
+
+  InitDialog();
+
+  document.getElementById("dataField").focus();
+
+  SetWindowLocation();
+}
+
+function InitDialog()
+{
+  document.getElementById("dataField").value = gDialog.theData;
+
+//  checkInaccessibleAcceleratorKeys(document.documentElement);
+
+//  gDialog.tabOrderArray = new Array( gDialog.positionSpecGroup,
+//                                       document.documentElement.getButton("accept"),
+//                                       document.documentElement.getButton("cancel") );
+  
+//  document.documentElement.getButton("accept").setAttribute("hidden", true);
+  document.documentElement.getButton("cancel").setAttribute("default", true);
+}
+
+function onAccept()
+{
+  data.Cancel = true;  //not that it matters
+  SaveWindowLocation();
+  return true;
+}
+
+function onCancel()
+{
+  data.Cancel = true;
+}
+
+function doAccept()
+{
+  document.documentElement.getButton('cancel').oncommand();
+}
---
+++ prince/base/content/dialogs/typesetNativeStyleOptions.js
@@ -0,0 +1,62 @@
+// Copyright (c) 2005 MacKichan Software, Inc.  All Rights Reserved.
+
+var gBodyElement;
+
+const emptyElementStr=" ";
+
+//const mmlns    = "http://www.w3.org/1998/Math/MathML";
+const xhtmlns  = "http://www.w3.org/1999/xhtml";
+
+var data;
+
+// dialog initialization code
+function Startup()
+{
+  var editor = GetCurrentEditor();
+  if (!editor) {
+    window.close();
+    return;
+  }
+
+  doSetOKCancel(onAccept, onCancel);
+  data = window.arguments[0];
+  data.Cancel = false;
+  gDialog.options = data.options;
+
+  InitDialog();
+
+  document.getElementById("optionsTextbox").focus();
+
+  SetWindowLocation();
+}
+
+function InitDialog()
+{
+  document.getElementById("optionsTextbox").value = gDialog.options.join(",");
+
+//  checkInaccessibleAcceleratorKeys(document.documentElement);
+
+//  gDialog.tabOrderArray = new Array( gDialog.positionSpecGroup,
+//                                       document.documentElement.getButton("accept"),
+//                                       document.documentElement.getButton("cancel") );
+  
+  document.documentElement.getButton("accept").setAttribute("default", true);
+}
+
+function onAccept()
+{
+  data.options = document.getElementById("optionsTextbox").value.split(",");
+
+  SaveWindowLocation();
+  return true;
+}
+
+function onCancel()
+{
+  data.Cancel = true;
+}
+
+function doAccept()
+{
+  document.documentElement.getButton('accept').oncommand();
+}
