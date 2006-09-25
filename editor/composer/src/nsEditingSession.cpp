746c2ae9488eb05b5c4f6c0401de4b8368d2c06e
MSI\barry <devnull@localhost>
2006-09-25 16:37:13 -0600

    Should have been committed with the others.

--- editor/composer/src/nsEditingSession.cpp
+++ editor/composer/src/nsEditingSession.cpp
@@ -1151,6 +1151,7 @@ InitialShell(nsString* initialURL)
   //BBM todo: check preferences and use this as a default only if necessary
   nsString url = NS_LITERAL_STRING("resource://app/res/shells/article/latexArticleShell.xhtml");
   *initialURL = url;
+  return NS_OK;
 }
 
 
