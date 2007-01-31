03023fe851b954ff6b235dd72843528e646bef28
MSI\barry <devnull@localhost>
2007-01-31 15:54:26 -0700

    Added OpenExtensions function

--- prince/base/content/msiEditor.js
+++ prince/base/content/msiEditor.js
@@ -4268,3 +4268,28 @@ function msiGoOnEvent(node, event) { msiCommandUpdater.onEvent(node, event); }
 //  }
 //}
 
+
+function OpenExtensions(aOpenMode)
+{
+  const EMTYPE = "Extension:Manager";
+  
+  var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                     .getService(Components.interfaces.nsIWindowMediator);
+  var needToOpen = true;
+  var windowType = EMTYPE + "-" + aOpenMode;
+  var windows = wm.getEnumerator(windowType);
+  while (windows.hasMoreElements()) {
+    var theEM = windows.getNext().QueryInterface(Components.interfaces.nsIDOMWindowInternal);
+    if (theEM.document.documentElement.getAttribute("windowtype") == windowType) {
+      theEM.focus();
+      needToOpen = false;
+      break;
+    }
+  }
+
+  if (needToOpen) {
+    const EMURL = "chrome://mozapps/content/extensions/extensions.xul?type=" + aOpenMode;
+    const EMFEATURES = "chrome,dialog=no,resizable";
+    window.openDialog(EMURL, "", EMFEATURES);
+  }
+}
