ae00b876528a609099b059dad35aa9a334fbd1da
Steve <devnull@localhost>
2007-02-28 09:34:18 -0700

    Implement caret left/right in matrices.

--- editor/msiediting/src/caret/msiMtableCaret.h
+++ editor/msiediting/src/caret/msiMtableCaret.h
@@ -11,15 +11,8 @@ class msiMtableCaret : public msiMCaretBase
 {
 public:
   msiMtableCaret(nsIDOMNode* mathmlNode, PRUint32 offset);
-  
-  // msiIMathmlCaret overrides
-  NS_IMETHOD  GetSelectableMathFragment(nsIEditor  *editor, 
-                                        nsIDOMNode *start,      PRUint32 startOffset, 
-                                        nsIDOMNode *end,        PRUint32 endOffset, 
-                                        nsIDOMNode **fragStart, PRUint32 *fragStartOffset,
-                                        nsIDOMNode **fragEnd,   PRUint32 *fragEndOffset);
-                                       
-  // end msiIMathmlCaret overrides
+  //msiIMathMLCaret
+  NS_DECL_MSIIMATHMLCARET
 };
 
 #endif // msiMtableCaret_h___
--- editor/msiediting/src/caret/msiMtrCaret.h
+++ editor/msiediting/src/caret/msiMtrCaret.h
@@ -5,12 +5,15 @@
 
 #include "nsCOMPtr.h"
 #include "nsIDOMNode.h"
-#include "msiMCaretBase.h"
+#include "msiMContainerCaret.h"
 
-class msiMtrCaret : public msiMCaretBase
+class msiMtrCaret : public msiMContainerCaret
 {
 public:
   msiMtrCaret(nsIDOMNode* mathmlNode, PRUint32 offset);
+
+  //msiIMathMLCaret
+  NS_DECL_MSIIMATHMLCARET
 };
 
 #endif // msiMtrCaret_h___
