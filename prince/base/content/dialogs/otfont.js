a348f7e2a5cf87975dc654906cd5b086790cd3de
SteveR <devnull@localhost>
2007-08-16 18:48:32 -0600

    Added a color command to Tags/Advanced

--- prince/base/content/dialogs/otfont.js
+++ prince/base/content/dialogs/otfont.js
@@ -19,63 +19,25 @@ function startup()
     }
   }
 }
-
-
-function switchUnits()
+function onAccept()
 {
-  currentUnit = document.getElementById("otfont.units").value;
-  document.getElementById('otfont.unit').setAttribute('value',currentUnit);
-}
-
-
-function handleChar(event, widget)
-{
-  var element = event.currentTarget;
-  if (event.keyCode == event.DOM_VK_UP)
-    goUp(widget);
-  else if (event.keyCode == event.DOM_VK_DOWN)
-    goDown(widget);
-}
-
-function goUp(widget)
-{
-  var value = Number(widget.value);
-  if (value == NaN) return;
-  var max = Number(widget.getAttribute("max"));
-  if ((max == 0)||(max == NaN)) max = Number.MAX_VALUE;
-  value += Number(widget.getAttribute("increment"));
-  value = Math.min(max,value);
-  widget.value = unitRound(value);
-}
-
-function goDown(widget)
-{
-  var value = Number(widget.value);
-  if (value == NaN) return;
-  var min = Number(widget.getAttribute("min"));
-  if (min == NaN) min = 0;
-  value -= Number(widget.getAttribute("increment"));
-  value = Math.max(min,value);
-  widget.value = unitRound(value);
-}
-
-
-function unitRound( size )
-{
-  var places;
-  // round off to a number of decimal places appropriate for the units
-  switch (currentUnit) {
-    case "mm" : places = 10;
-      break;
-    case "cm" : places = 100;
-      break;
-    case "pt" : places = 10;
-      break;
-    case "in" : places = 100;
-      break;
-    default   : places = 100;
+  var list = document.getElementById("otfontlist");
+  var fontname;
+  if (list.selectedIndex  > 0)
+  {
+    fontname = list.value;
+    var editorElement = msiGetParentEditorElementForDialog(window);
+    if (!editorElement)
+    {
+    AlertWithTitle("Error", "No editor in otfont.OnAccept!");
+    }
+    msiEditorSetTextProperty(editorElement, "otfont", "fontname", fontname);
   }
-  return Math.round(size*places)/places;
+  editorElement.contentWindow.focus();
 }
 
+function onCancel()
+{
+  return true;
+}
 
