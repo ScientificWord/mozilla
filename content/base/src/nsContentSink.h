0dc4c7ac4bdbfad5c722113ca98446cc56e7db57
MSI\barry <devnull@localhost>
2007-09-07 14:57:27 -0600

    Files changed by manually merging v11 of the patch for Mozilla bug 18333, (XML Content Sink should be incremental)

--- content/base/src/nsContentSink.h
+++ content/base/src/nsContentSink.h
@@ -52,6 +52,13 @@
 #include "nsCOMArray.h"
 #include "nsString.h"
 #include "nsAutoPtr.h"
+#include "nsITimer.h"
+#include "nsStubDocumentObserver.h"
+#include "nsIParserService.h"
+#include "nsIContentSink.h"
+#include "prlog.h"
+#include "nsIRequest.h"
+
 
 class nsIDocument;
 class nsIURI;
@@ -64,19 +71,76 @@ class nsIChannel;
 class nsIContent;
 class nsIViewManager;
 class nsNodeInfoManager;
+#ifdef NS_DEBUG
+
+extern PRLogModuleInfo* gContentSinkLogModuleInfo;
+
+#define SINK_TRACE_CALLS              0x1
+#define SINK_TRACE_REFLOW             0x2
+#define SINK_ALWAYS_REFLOW            0x4
+
+#define SINK_LOG_TEST(_lm, _bit) (PRIntn((_lm)->level) & (_bit))
+
+#define SINK_TRACE(_lm, _bit, _args) \
+  PR_BEGIN_MACRO                     \
+    if (SINK_LOG_TEST(_lm, _bit)) {  \
+      PR_LogPrint _args;             \
+    }                                \
+  PR_END_MACRO
+
+#else
+#define SINK_TRACE(_lm, _bit, _args)
+#endif
+
+#undef SINK_NO_INCREMENTAL
+
+//----------------------------------------------------------------------
+
+// 1/2 second fudge factor for window creation
+#define NS_DELAY_FOR_WINDOW_CREATION  500000
+
+// 200 determined empirically to provide good user response without
+// sampling the clock too often.
+#define NS_MAX_TOKENS_DEFLECTED_IN_LOW_FREQ_MODE 200
+
 
 class nsContentSink : public nsICSSLoaderObserver,
                       public nsIScriptLoaderObserver,
-                      public nsSupportsWeakReference
+                      public nsSupportsWeakReference,
+                      public nsStubDocumentObserver,
+                      public nsITimerCallback
 {
+  friend class DummyParserRequest;
+
   NS_DECL_ISUPPORTS
   NS_DECL_NSISCRIPTLOADEROBSERVER
+
+    // nsITimerCallback
+  NS_DECL_NSITIMERCALLBACK
+
   
   // nsICSSLoaderObserver
   NS_IMETHOD StyleSheetLoaded(nsICSSStyleSheet* aSheet, PRBool aWasAlternate,
                               nsresult aStatus);
 
   nsresult ProcessMETATag(nsIContent* aContent);
+  // nsIContentSink impl
+  NS_IMETHOD WillInterruptImpl(void);
+  NS_IMETHOD WillResumeImpl(void);
+  NS_IMETHOD DidProcessATokenImpl(void);
+  NS_IMETHOD WillBuildModelImpl(void);
+  NS_IMETHOD DidBuildModelImpl(void);
+  NS_IMETHOD DropParserAndPerfHint(void);
+  NS_IMETHOD WillProcessTokensImpl(void);
+
+  void NotifyAppend(nsIContent* aContent, PRUint32 aStartIndex);
+
+  // nsIDocumentObserver
+  virtual void BeginUpdate(nsIDocument *aDocument, nsUpdateType aUpdateType);
+  virtual void EndUpdate(nsIDocument *aDocument, nsUpdateType aUpdateType);
+
+  virtual void UpdateChildCounts() = 0;
+
 
 protected:
   nsContentSink();
@@ -106,10 +170,43 @@ protected:
   PRBool ScrollToRef(PRBool aReallyScroll);
   nsresult RefreshIfEnabled(nsIViewManager* vm);
   void StartLayout(PRBool aIsFrameset);
+  PRBool IsTimeToNotify();
+
+  void
+  FavorPerformanceHint(PRBool perfOverStarvation, PRUint32 starvationDelay);
+
+  inline PRInt32 GetNotificationInterval()
+  {
+    if (mDynamicLowerValue) {
+      return 1000;
+    }
+
+    return mNotificationInterval;
+  }
+
+  inline PRInt32 GetMaxTokenProcessingTime()
+  {
+    if (mDynamicLowerValue) {
+      return 3000;
+    }
+
+    return mMaxTokenProcessingTime;
+  }
+
 
   // Overridable hooks into script evaluation
   virtual void PreEvaluateScript()                            {return;}
   virtual void PostEvaluateScript(nsIScriptElement *aElement) {return;}
+  virtual nsresult FlushTags(PRBool aNotify){return NS_OK;}
+
+  virtual void TryToScrollToRef()
+  { return;
+  }
+
+  // CanInterrupt parsing related routines
+  nsresult AddDummyParserRequest(void);
+  nsresult RemoveDummyParserRequest(void);
+
 
   nsCOMPtr<nsIDocument>         mDocument;
   nsCOMPtr<nsIParser>           mParser;
@@ -123,6 +220,63 @@ protected:
 
   nsCString mRef; // ScrollTo #ref
   PRBool mNeedToBlockParser;
+
+  // back off timer notification after count
+  PRInt32 mBackoffCount;
+
+  // Notification interval in microseconds
+  PRInt32 mNotificationInterval;
+
+  // Time of last notification
+  PRTime mLastNotificationTime;
+
+  // Timer used for notification
+  nsCOMPtr<nsITimer> mNotificationTimer;
+
+  // Do we notify based on time?
+  PRPackedBool mNotifyOnTimer;
+
+  PRPackedBool mLayoutStarted;
+  PRPackedBool mScrolledToRefAlready;
+
+  PRUint8 mScriptEnabled : 1;
+  PRUint8 mFramesEnabled : 1;
+  PRUint8 mCanInterruptParser : 1;
+  PRUint8 mDynamicLowerValue : 1;
+  PRUint8 mFormOnStack : 1;
+  PRUint8 mParsing : 1;
+  PRUint8 mDroppedTimer : 1;
+
+  // -- Can interrupt parsing members --
+  PRUint32 mDelayTimerStart;
+
+  // Interrupt parsing during token procesing after # of microseconds
+  PRInt32 mMaxTokenProcessingTime;
+
+  // Switch between intervals when time is exceeded
+  PRInt32 mDynamicIntervalSwitchThreshold;
+
+  PRInt32 mBeginLoadTime;
+
+  // Last mouse event or keyboard event time sampled by the content
+  // sink
+  PRUint32 mLastSampledUserEventTime;
+
+  // The number of tokens that have been processed while in the low
+  // frequency parser interrupt mode without falling through to the
+  // logic which decides whether to switch to the high frequency
+  // parser interrupt mode.
+  PRUint8 mDeflectedCount;
+
+  // Boolean indicating whether we've notified insertion of our root content
+  // yet.  We want to make sure to only do this once.
+  PRPackedBool mNotifiedRootInsertion;
+
+  PRInt32 mInMonolithicContainer;
+
+  PRInt32 mInNotification;
+
+  nsCOMPtr<nsIRequest> mDummyParserRequest;
 };
 
 #endif // _nsContentSink_h_
