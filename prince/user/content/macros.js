907b45bb10b0d351094348b59465049f931acea6
barry <devnull@localhost>
2008-02-06 17:08:37 -0700

    Minor fixes. Added a routine that will dump a math expression to the console, showing where the cursor is (assigned to alt-F12) Added a way to call for a soft-save (currently assigned to alt-F11).

--- prince/user/content/macros.js
+++ prince/user/content/macros.js
@@ -11,8 +11,9 @@ function deleteSelection()
   msiGoDoCommand('cmd_delete');
 }
 
-function insertMathSymbol( s )
+function insertMathSymbol( s, delSelection )
 {
+  if (delSelection) deleteSelection();
   doParamCommand('cmd_MSIsymbolCmd',s);
 }
 
@@ -94,5 +95,89 @@ function previewDVI(delSelection)
 {
   if (delSelection) deleteSelection();
   printTeX(false,true);
+}
+
+// The following was added by BBM for diagnostic purposes. It should not be in the release version
+// If the selection is collapsed and in a math object, this will dump the math object and show
+// the location of the selection point.
+
+function dumpMath()
+{
+  var editorElement = document.getElementById("content-frame");
+  var editor = msiGetEditor(editorElement);
+  var HTMLEditor = editor.QueryInterface(Components.interfaces.nsIHTMLEditor);
+  var rootnode = HTMLEditor.getSelectionContainer();
+  while (rootnode && rootnode.localName != "math") rootnode = rootnode.parentNode;
+  if (!rootnode)
+  { 
+    dump("Failed to find math node\n");
+    return;
+  }
+  var sel = HTMLEditor.selection;
+  var selNode = sel.anchorNode;
+  var selOffset = sel.anchorOffset;
+  var indent = 0;
+  dump("Selection: node="+selNode.nodeType == Node.TEXT_NODE?"text":selNode.localName+", offset="+selOffset+"\n");
+  dumpNodeMarkingSel(rootnode, selNode, selOffset, indent);
+}
+
+
+function doIndent( k )
+{
+  for (j = 0; j < k; j++) dump("  ");
+}
+
+
+function dumpNodeMarkingSel(node, selnode, offset, indent)
+{
+  var len = node.childNodes.length;
+  if (node.nodeType == Node.ELEMENT_NODE)
+  {
+    doIndent(indent);
+    dump("<"+((node.localName!="null")?node.localName:"text")+"> \n");
+    for (var i = 0; i < len; i++)
+    {    
+      if (node==selnode && i==offset)
+      {
+        for (var j = 0; j<= indent; j++) dump("**"); 
+        dump("<cursor>\n");
+      }
+      dumpNodeMarkingSel(node.childNodes[i],selnode,offset, indent+1);
+    }
+    if (node==selnode && offset==len) 
+    {
+      for (var j = 0; j<= indent; j++) dump("**"); 
+      dump("<cursor>\n");
+    }
+    doIndent(indent);
+    dump("</"+node.localName+">\n");
+  }
+  else if (node.nodeType == Node.TEXT_NODE)
+  {
+    if (node==selnode)
+    {
+      doIndent(indent);
+      dump(node.nodeValue.slice(0,offset));
+      dump("<cursor>");
+      dump(node.nodeValue.slice(offset)+"\n");
+    }
+    else 
+    {
+      var s = node.nodeValue;
+      var t = s.replace(/^\s*/,'');
+      var r = t.replace(/\s*$/,'');
+      if (r.length>0)
+      {
+        doIndent(indent);
+        dump(r+'\n');
+      }
+    }  
+  }
+}   
+  
+
+function softSave()
+{
+  msiGoDoCommand('cmd_softSave');
 }
 
