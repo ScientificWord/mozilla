85c2d56742ba2bda291398ecd01458a0baf915d0
KORURANGI\Ron <devnull@localhost>
2008-05-16 02:32:10 -0600

    Changed the value of "swp.defaultDialogShell" to use the new StdDialogShell.sci.

--- prince/prince-prefs.js
+++ prince/prince-prefs.js
@@ -15,7 +15,7 @@ pref("toolkit.scrollbar.scrollIncrement", 20);
 pref("toolkit.scrollbar.smoothscroll", true);
 
 pref("swp.defaultShell", "article/StdArticleShell.sci");
-pref("swp.defaultDialogShell", "article/LaTeXDialogShell.sci");
+pref("swp.defaultDialogShell", "article/StdDialogShell.sci");
 pref("swp.saveintervalminutes",2);
 pref("editor.author",                       "");
 
