61ac5c2167facdcba51fcb5ac89272a3e51cac6d
barry <devnull@localhost>
2008-08-04 19:29:00 -0600

    Added an assertion that this file should not be used (it is replaced by msiEditor.js)

--- editor/ui/composer/content/editor.js
+++ editor/ui/composer/content/editor.js
@@ -497,6 +497,7 @@ var gEditorDocumentObserver =
           // Force color widgets to update
           onFontColorChange();
           onBackgroundColorChange();
+          alert("We shouldn't be using editor.js!")e
           editor.addTagInfo("resource:///res/tagdefs/latexdefs.xml");
           // the order here is important, since the autocomplete component has to read the tag names 
         }
