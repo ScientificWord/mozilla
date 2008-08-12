9b5dc31652d96b6e464731dd2e07fcfc6103756e
barry <devnull@localhost>
2008-08-12 15:00:03 -0600

    Changed this, but the default command window on your computer must have the path set for your TeX executables.

--- prince/TeX/bin/pdflatex.cmd
+++ prince/TeX/bin/pdflatex.cmd
@@ -1,4 +1,4 @@
 @echo off
 pushd %2
-for /L %%i in (1,1,%4) do echo Pass %%i & echo. & "c:\program files\miktex 2.7\miktex\bin\xelatex.exe" %3 %5 %6 %7 %8 %9
+for /L %%i in (1,1,%4) do echo Pass %%i & echo. & "xelatex.exe" %3 %5 %6 %7 %8 %9
 popd
