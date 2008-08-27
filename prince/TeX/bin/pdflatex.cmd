9eac8b9dda1a4c16f35b4821052901f83750a5cb
barry <devnull@localhost>
2008-08-27 16:26:34 -0600

    Fixed conflicts

--- prince/TeX/bin/pdflatex.cmd
+++ prince/TeX/bin/pdflatex.cmd
@@ -1,5 +1,5 @@
 @echo off
 pushd %2
-path="c:\program files\texlive\2008\bin\win32"
+set path="C:\program files\miktex 2.7\miktex\bin"
 for /L %%i in (1,1,%4) do echo Pass %%i & echo. & "xelatex.exe" %3 %5 %6 %7 %8 %9
 popd
