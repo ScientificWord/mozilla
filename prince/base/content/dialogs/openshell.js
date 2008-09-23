87698ade230badb4652ff2fff752cc348e1bade3
barry <devnull@localhost>
2008-09-22 22:51:04 -0600

    Made a new Select Shell dialog to match SWP5.5 (instead of using the usual open file dialog).

---
+++ prince/base/content/dialogs/openshell.js
@@ -0,0 +1,71 @@
+function initialize()
+{
+  var url;
+  var dsprops = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties);
+  var dir =dsprops.get("resource:app", Components.interfaces.nsIFile);
+  dir.append("shells");
+  url = dir.path;
+  // is this platform independent?
+  url.replace("\\","/","g");
+  if (url.charAt(0) === '/')
+    url = "file://"+url;
+  else
+    url = "file:///"+url;
+  var tree = document.getElementById("dir-tree");
+  tree.setAttribute("ref", url);
+  tree.currentIndex = 0;
+  showShellsInDir(tree);
+}                           
+
+function showShellsInDir(tree)
+{
+  var regexp = /\.sci$/i;
+  var namecol = tree.columns.getNamedColumn('Name');
+  var i = tree.currentIndex;
+  var leafname = tree.view.getCellText( i,namecol);
+  while (tree.view.getParentIndex(i) >= 0)
+  {           
+    i = tree.view.getParentIndex(i);
+    leafname = tree.view.getCellText(i,namecol)+ "/" + leafname;
+  }
+  var directory = Components.classes["@mozilla.org/file/local;1"].           
+  createInstance(Components.interfaces.nsILocalFile);
+  var path= tree.getAttribute("ref")+"/"+leafname;
+  //convert URL to path
+  path = path.replace(/^file:\/\/\//i,"");
+  //for windows
+  path = path.replace("/",'\\','g');
+  try {
+    directory.initWithPath(path);
+//    directory.append(leafname);
+    var items = directory.directoryEntries;
+    var name;  
+    var listbox = document.getElementById("dircontents");
+    while (listbox.itemCount > 0) listbox.removeItemAt(0);
+    while (items.hasMoreElements()) {
+      var item = items.getNext().QueryInterface(Components.interfaces.nsIFile);
+      if (item.isFile() && regexp.test(item.leafName))
+      {
+        name = item.leafName.replace(regexp,'');
+        listbox.appendItem(name, item.path);
+      }
+    }
+    listbox.selectedIndex =0; 
+  }
+  catch(e) {
+    dump(e.toString());
+  }
+}
+
+function onAccept()
+{
+  window.arguments[0].filename = document.getElementById("dircontents").value;
+  return true;
+}
+
+
+function onCancel()
+{
+  window.arguments[0].filename = null;
+  return true;
+}
