3bc50a67ae27a9713ad41eb3d6b5e173385a3dbb
tiuka\daveb <devnull@localhost>
2009-02-05 08:30:42 -0700

    Update to mozilla.org for Mac build

--- toolkit/xre/nsXREDirProvider.cpp
+++ toolkit/xre/nsXREDirProvider.cpp
@@ -287,12 +287,6 @@ nsXREDirProvider::GetFile(const char* aProperty, PRBool* aPersistent,
     rv = GetAppDir()->Clone(getter_AddRefs(file));
   }
 
-  else if (!strcmp(aProperty, "resource:docdir")) {
-    *aPersistent = PR_FALSE;
-    printf("Looking for resource:docdir\n");
-    rv = NS_OK;
-  }
-
   else if (!strcmp(aProperty, NS_APP_PROFILE_DIR_STARTUP) && mProfileDir) {
     return mProfileDir->Clone(aFile);
   }
