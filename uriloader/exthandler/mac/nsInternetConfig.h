be361abbe9c06d4d62f4e3bc30efc36f310d2306
tiuka\daveb <devnull@localhost>
2009-02-05 08:30:42 -0700

    Update to mozilla.org for Mac build

--- uriloader/exthandler/mac/nsInternetConfig.h
+++ uriloader/exthandler/mac/nsInternetConfig.h
@@ -35,88 +35,25 @@
  *
  * ***** END LICENSE BLOCK ***** */
  
-#include "nsInternetConfig.h"
-#include "nsString.h"
-#include "nsReadableUtils.h"
-#include "nsDebug.h"
+#ifndef __NS_INTERNETCONFIG_H__
+#define __NS_INTERNETCONFIG_H__
 
-#include <CodeFragments.h>   
-#include <Processes.h>
-ICInstance nsInternetConfig::sInstance = NULL;
-long nsInternetConfig::sSeed = 0;
-PRInt32  nsInternetConfig::sRefCount = 0;
+#include <InternetConfig.h>
+#include "prtypes.h"
+#include "nsError.h"
 
-
-
-static OSType GetAppCreatorCode()
+class nsInternetConfig
 {
-  ProcessSerialNumber psn = { 0, kCurrentProcess } ;
-  ProcessInfoRec      procInfo;
-  
-  procInfo.processInfoLength = sizeof(ProcessInfoRec);
-  procInfo.processName = nsnull;
-  procInfo.processAppSpec = nsnull;
-  
-  GetProcessInformation(&psn, &procInfo);
-  return procInfo.processSignature;  
-}
+public:
+	nsInternetConfig();
+	~nsInternetConfig();
 
+	static ICInstance GetInstance();
+	static  PRBool		HasSeedChanged();
+private:
+	static	ICInstance sInstance;
+	static  long sSeed;
+	static  PRInt32 sRefCount;
+};
 
-
-ICInstance nsInternetConfig::GetInstance()
-{
-	if ( !sInstance )
-	{
-		OSStatus err;
-		if ((long)ICStart == kUnresolvedCFragSymbolAddress )
-			return sInstance;                          
-                                                                                 
-                                                                                  
-		OSType creator = GetAppCreatorCode();
-		err = ::ICStart( &sInstance, creator  );
-		if ( err != noErr )
-		{
-			::ICStop( sInstance );
-		}
-		else
-		{
-#if !TARGET_CARBON
-			::ICFindConfigFile( sInstance, 0 , nil );
-#endif
-			::ICGetSeed( sInstance, &sSeed );
-		}
-	}
-	return sInstance;
-}
-
-PRBool nsInternetConfig::HasSeedChanged()
-{
-	ICInstance instance = nsInternetConfig::GetInstance();
-	if ( instance )
-	{
-		long newSeed = 0;
-		::ICGetSeed( sInstance, &newSeed );
-		if ( newSeed != sSeed )
-		{
-			sSeed = newSeed;
-			return PR_TRUE;
-		}
-	}
-	return PR_FALSE;
-}
-
-nsInternetConfig::nsInternetConfig()
-{
-	sRefCount++;
-}
-
-nsInternetConfig::~nsInternetConfig()
-{
-	sRefCount--;
-	if ( sRefCount == 0 && sInstance)
-	{
-		::ICStop( sInstance );
-		sInstance = NULL;
-	}
-}
-
+#endif /* __NS_INTERNETCONFIG_H__ */
