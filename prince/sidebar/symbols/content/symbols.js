a48c79e55cb0bec69425bee9e4365138e2ad6886
barry <devnull@localhost>
2009-04-10 19:02:52 -0600

    Added symbol palettes to the sidebar

---
+++ prince/sidebar/symbols/content/symbols.js
@@ -0,0 +1,16 @@
+function doSymbols(event)
+{
+  var w = window.parent;
+  if (!("doParamCommand" in w)) w = w.opener;
+  var editorElement = msiGetActiveEditorElement(w);
+  editorElement.contentWindow.focus();
+  // this will get to the right command when the tabs are in the sidebar or detached
+  try
+  {
+    var cmdParams = newCommandParams();
+    if (!cmdParams) return;
+
+    cmdParams.setStringValue("value", event.target.getAttribute('msivalue'));
+    msiGoDoCommandParams("cmd_MSIsymbolCmd", cmdParams);
+  } catch(e) { dump("error thrown in doSymbols: "+e+"\n"); }
+}
