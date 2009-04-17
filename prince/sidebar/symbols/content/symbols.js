f32d607fb048d3c42f8d9adfe6ce4450114818a4
barry <devnull@localhost>
2009-04-17 15:52:23 -0600

    Changed the sidebar symbol panel so that math characters appear in red and text characters in black. Also changed it so that text characters are inserted as text, not math.

--- prince/sidebar/symbols/content/symbols.js
+++ prince/sidebar/symbols/content/symbols.js
@@ -14,3 +14,19 @@ function doSymbols(event)
     msiGoDoCommandParams("cmd_MSIsymbolCmd", cmdParams);
   } catch(e) { dump("error thrown in doSymbols: "+e+"\n"); }
 }
+
+
+function doChar(event)
+{
+  var w = window.parent;
+  if (!("doParamCommand" in w)) w = w.opener;
+  var editorElement = msiGetActiveEditorElement(w);
+  editorElement.contentWindow.focus();
+  // this will get to the right command when the tabs are in the sidebar or detached
+  var editor = msiGetEditor(editorElement);
+  try
+  {
+    var s = event.target.getAttribute('msivalue');
+    editor.insertText(s);
+  } catch(e) { dump("error thrown in doSymbols: "+e+"\n"); }
+}
