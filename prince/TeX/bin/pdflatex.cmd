624a701ba31bf5e9796dc4b230865a909e99cb2e
barry <devnull@localhost>
2009-06-24 15:29:22 -0600

    Modifications so preview PDF now works, and switching to the PDF Preview tab builds the pdf file if necessary. Still working on getting Acrobat to play nicely with us.

--- prince/TeX/bin/pdflatex.cmd
+++ prince/TeX/bin/pdflatex.cmd
@@ -1,5 +1,5 @@
-@echo off
+rem @echo off
 pushd %2
 set path="C:\program files\miktex 2.7\miktex\bin"
-for /L %%i in (1,1,%4) do echo Pass %%i & echo. & "xelatex.exe" %3 %5 %6 %7 %8 %9
+for /L %%i in (1,1,%5) do echo Pass %%i & echo. & "xelatex.exe" -job-name=%4 %3 %6 %7 %8 %9
 popd
