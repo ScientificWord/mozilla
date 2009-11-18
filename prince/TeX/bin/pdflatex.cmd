356e394a2458847bab8050439b7e3552c1693ee7
barry <devnull@localhost>
2009-11-18 09:45:24 -0700

    Updated the pdflatex.cmd file. It was failing on Vista because MS changed the name of the "Program Files" directory to "Program Files (x86)". I also switched over to TexLive 2009 from Miktex 2.8. This assumes that you have installed texlive in %programfiles%\texlive\2009.
    All of this will eventually come from user settings.

--- prince/TeX/bin/pdflatex.cmd
+++ prince/TeX/bin/pdflatex.cmd
@@ -1,5 +1,5 @@
 rem @echo off
 pushd %2
-set path="C:\program files\miktex 2.8\miktex\bin"
-"xelatex.exe" -job-name=%4 %3 %6 %7 %8 %9
+set path="%programfiles%\texlive\2009\bin\win32"
+xelatex -jobname=%4 %3 %6 %7 %8 %9
 popd
