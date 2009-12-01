4562d2c1ca898e9f2064e4b95ba49f609c9c393d
tiuka\jon <devnull@localhost>
2009-12-01 15:06:51 -0700

    Moving compute stuff to mozilla toolkit Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- toolkit/toolkit-tiers.mk
+++ toolkit/toolkit-tiers.mk
@@ -136,6 +136,8 @@ tier_gecko_dirs	+= \
 		toolkit/components/scriptrunner \
 		toolkit/components/autocomplete/public \
 		toolkit/components/autosub/public \
+		toolkit/components/Compute/public \
+	    toolkit/components/Wrappers/public \
 		toolkit/components/keymap/public \
 		editor \
 		layout \
