3b2e22bc99a553a327e3ee05c6ba71326e4e7f17
Barry MacKichan <barry.mackichan@mackichan.com>
2010-07-02 17:37:55 -0600

    Found about the "enabledelayedexpansion" keyword in batch files, which makes it run much better on Windows XP, where the documents directory has a space in its path.

--- prince/TeX/bin/pdflatex.cmd
+++ prince/TeX/bin/pdflatex.cmd
@@ -1,6 +1,7 @@
 rem @echo off
+setlocal enabledelayedexpansion
 pushd %2
 set path="%MSITEXBIN%"
-xelatex -jobname=%4 %3 %6 %7 %8 %9
+xelatex -interaction=nonstopmode -jobname=%4 %3 %6 %7 %8 %9
 echo done > sentinel
 popd
