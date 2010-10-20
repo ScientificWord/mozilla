b1806dd522e4c280e821ef147c421ae4828fde33
Barry MacKichan <barry.mackichan@mackichan.com>
2010-10-20 17:19:24 -0600

    Fixed a blunder that forces all math symbols to be mi´s.

--- editor/msiediting/src/msiEditingManager.cpp
+++ editor/msiediting/src/msiEditingManager.cpp
@@ -736,7 +736,7 @@ msiEditingManager::InsertSymbol(nsIEditor * editor,
   {
     nsCOMPtr<nsIDOMElement> mathmlElement;
     PRUint32 flags(msiIMathMLInsertion::FLAGS_NONE);
-    res = msiUtils::CreateMathMLLeafElement(editor, symbol, msiIMathMLEditingBC::MATHML_MI, 1, flags, mathmlElement);
+    res = msiUtils::CreateMathMLLeafElement(editor, symbol, 1, flags, mathmlElement);
     if (NS_SUCCEEDED(res) && mathmlElement)
       res = InsertMathmlElement(editor, selection, node, offset, flags, mathmlElement);
   }
