a95bd5c2a4a80b60dfb9936a6df450bd15fa93d9
Ron Ausbrooks <ron.ausbrooks@mackichan.com>
2010-12-03 03:17:05 -0700

    Fixes to structure properties (Section properties) dialog. Also added editor control and other support for the LaTeX short title for sections, though some of this may not be working properly yet.

--- prince/base/content/dialogs/structureproperties.js
+++ prince/base/content/dialogs/structureproperties.js
@@ -1,5 +1,5 @@
-var structureData;
-var element;
+var structureData = new Object();
+//var structNode;
 
 function capitalize( str )
 {
@@ -10,44 +10,72 @@ function capitalize( str )
 
 function startup()
 {
-  structureData = window.arguments[0];
+//  structureData = window.arguments[0];
+  structureData.structNode = window.arguments[0];
+  if (!structureData.structNode)
+    return;
+  var tagName = msiGetBaseNodeName(structureData.structNode);
+
+  var sectitle = structureData.structNode.firstChild;
+  while (sectitle && sectitle.nodeType != Node.ELEMENT_NODE)
+    sectitle = sectitle.nextSibling;
+  structureData.secTitleNode = sectitle;  //save for later
   dump("structureproperties startup()\n");
-  document.title = capitalize(document.title.replace("##", structureData.tagName));
+
+  document.title = capitalize(document.title.replace("##", tagName));
   var str;
   str = capitalize(document.getElementById("textboxlabel").getAttribute("value"));
-  document.getElementById("textboxlabel").setAttribute("value",
-    capitalize(str.replace("##", structureData.tagName)));
+  document.getElementById("textboxlabel").setAttribute("value", capitalize(str.replace("##", tagName)));
   str = document.getElementById("numberedCheckbox").getAttribute("label");
-  document.getElementById("numberedCheckbox").setAttribute("label",
-    capitalize(str.replace("##", structureData.tagName)));
+  document.getElementById("numberedCheckbox").setAttribute("label", capitalize(str.replace("##", tagName)));
+
+  var shortFormEditor = document.getElementById("sectionShortEdit");
   try {
-    var editorElement = msiGetParentEditorElementForDialog(window);  
-    var mixed = msiGetSelectionContainer(editorElement);
-    if (!mixed) return;
-    element = mixed.node;
-    if (!element) return;
-    while (element && element.localName != structureData.tagName) 
-      element = element.parentNode;
+//  var editorElement = msiGetParentEditorElementForDialog(window);  
+//  var mixed = msiGetSelectionContainer(editorElement);
+//  if (!mixed) return;
+//  element = mixed.node;
+//  if (!element) return;
+//  while (element && element.localName != structureData.tagName) 
+//  element = element.parentNode;
     var filename;
-    if (element)
+    var children;
+    // save the starting state
+    if (structureData.structNode.hasAttribute("open"))
+      structureData.open = structureData.structNode.getAttribute("open")=="true" ? true : false;
+    if (sectitle)
+      children = msiNavigationUtils.getSignificantContents(sectitle);
+    for (var ix = 0; !structureData.shortFormNode && (ix < children.length); ++ix)
     {
-      // save the starting state
-      if (element.hasAttribute("open")) structureData.open = element.getAttribute("open")=="true"?true:false;
-      if (element.hasAttribute("subdoc"))
-      {
-        document.getElementById("subdocCheckbox").checked = true;
-        filename = element.getAttribute("subdoc");
-        structureData.subdoc = filename;
-        if (!(filename && filename.length > 0))
-          filename = generateFilename(element);
-        //validate here??
-        document.getElementById("textboxlabel").disabled = false;
-        document.getElementById("subdocFileName").disabled = false;
-        document.getElementById("subdocFileName").value = filename;
-      } else document.getElementById("subdocCheckbox").checked = false;
-      if (element.hasAttribute("nonum"))
-        document.getElementById("numberedCheckbox").checked = false;
+      if (msiGetBaseNodeName(children[ix]) == "shortTitle")
+        structureData.shortFormNode = children[ix];
     }
+    structureData.shortFormStr = "";
+    if (structureData.shortFormNode)
+    {
+      var serializer = new XMLSerializer();
+      structureData.shortFormStr = serializer.serializeToString(structureData.shortFormNode);
+    }
+    var editorInitializer = new msiEditorArrayInitializer();
+    editorInitializer.addEditorInfo(shortFormEditor, structureData.shortFormStr, true);
+    editorInitializer.doInitialize();
+
+    if (structureData.structNode.hasAttribute("subdoc"))
+    {
+      document.getElementById("subdocCheckbox").checked = true;
+      filename = structureData.structNode.getAttribute("subdoc");
+      structureData.subdoc = filename;
+      if (!(filename && filename.length > 0))
+        filename = generateFilename(structureData.structNode);
+      //validate here??
+      document.getElementById("textboxlabel").disabled = false;
+      document.getElementById("subdocFileName").disabled = false;
+      document.getElementById("subdocFileName").value = filename;
+    }
+    else
+      document.getElementById("subdocCheckbox").checked = false;
+    if (structureData.structNode.hasAttribute("nonum"))
+      document.getElementById("numberedCheckbox").checked = false;
   }
   catch(e) {
     dump("Error in structureproperties.js, startup: "+e.message+"\n");
@@ -122,27 +150,45 @@ function onCancel() {
 
 function onAccept()
 {
-  var sectitle = element.firstChild;
-  while (sectitle && sectitle.nodeType != Node.ELEMENT_NODE) sectitle = sectitle.nextSibling;
-  if (sectitle)
-  { 
-    // if it is currently written to disk, and this is changing, or the file name is changing, read it back in
-    if (!structureData.open && structureData.subdoc && 
-      (!element.hasAttribute("subdoc") || element.getAttribute("subdoc") != structureData.subdoc))
-      sectitle.getFromDisk(structureData.subdoc);
-    if (element.hasAttribute("subdoc") && element.getAttribute("open") == "false")  // then we need to write the contents to disk.
-    {                                                                   
-      sectitle.saveToDisk(element.getAttribute("subdoc"));
+  var reviseData = new Object();
+
+  var sectitle = structureData.secTitleNode;
+//  while (sectitle && sectitle.nodeType != Node.ELEMENT_NODE)
+//    sectitle = sectitle.nextSibling;
+  var shortFormEditor = document.getElementById("sectionShortEdit");
+  var editorElement = msiGetParentEditorElementForDialog(window);
+  var parentEditor = msiGetEditor(editorElement);
+  try
+  {
+    if (sectitle)
+    { 
+      // if it is currently written to disk, and this is changing, or the file name is changing, read it back in
+      if (!structureData.open && structureData.subdoc && 
+        (!structureData.structNode.hasAttribute("subdoc") || structureData.structNode.getAttribute("subdoc") != structureData.subdoc))
+        sectitle.getFromDisk(structureData.subdoc);
+      if (structureData.structNode.hasAttribute("subdoc") && structureData.structNode.getAttribute("open") == "false")  // then we need to write the contents to disk.
+      {                                                                   
+        sectitle.saveToDisk(structureData.structNode.getAttribute("subdoc"));
+      }
+      reviseData.subDocName = "";
+      if (document.getElementById("subdocCheckbox").checked)
+        reviseData.subDocName = document.getElementById("subdocFileName").value;
+
+      reviseData.noNumAttr = "";
+      if (!document.getElementById("numberedCheckbox").checked)
+        reviseData.noNumAttr = "true";
+
+      var shortFormContentFilter = new msiDialogEditorContentFilter(shortFormEditor);
+      reviseData.newShortForm = shortFormContentFilter.getDocumentFragmentString();
+      dump("In structure properties dialog, short form title is [" + reviseData.newShortForm + "].\n");
     }
-    if (document.getElementById("subdocCheckbox").checked)
-      element.setAttribute("subdoc",document.getElementById("subdocFileName").value);
-    else 
-      element.removeAttribute("subdoc");
-    if (!document.getElementById("numberedCheckbox").checked)
-      element.setAttribute("nonum","true");
-    else
-      element.removeAttribute("nonum"); 
-    
+  
+    var theWindow = window.opener;
+    if (!theWindow || !("doReviseStructureNode" in theWindow))
+      theWindow = msiGetTopLevelWindow();
+    if (parentEditor && theWindow)
+      theWindow.doReviseStructureNode(parentEditor, structureData, reviseData);
   }
+  catch(exc) {dump("In structureproperties dialog onAccept(), exception: [" + exc + "].\n");}
   return true;    
 }
