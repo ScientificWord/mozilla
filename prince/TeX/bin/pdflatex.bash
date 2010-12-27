47163fcf5ee608342edbc5bcc54e7f46e3503f1a
Barry MacKichan <barry.mackichan@mackichan.com>
2010-12-27 12:54:33 -0700

    Changes to allow pdfTeX to run on the Mac

--- prince/TeX/bin/pdflatex.bash
+++ prince/TeX/bin/pdflatex.bash
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # $1 is a legacy argument, no longer used
-pushd $2
-pdflatex -interaction=nonstopmode -jobname=$4 $3 $6 $7 $8 $9
+pushd $2 > /Users/barry/pdftex.log
+echo "Calling pdflatex -interaction=nonstopmode -jobname=$4 $3 $6 $7 $8 $9"  >> /Users/barry/pdftex.log
+/usr/texbin/pdflatex -interaction=nonstopmode -jobname=$4 $3 $6 $7 $8 $9 >> /Users/barry/pdftex.log 2>&1
 echo done > sentinel
 popd
 
