e7283fbe35f2927304f64af57c138bd044aa98e3
Dave Bailey <daveb@mackichan.com>
2011-08-16 12:00:00 -0600

    Build font family list on Mac and Linux.

--- prince/bin/OSX/BuildFontFamilyList.bash
+++ prince/bin/OSX/BuildFontFamilyList.bash
@@ -1,3 +1,4 @@
+#!/bin/sh
 # build list of family names of ttf and otf fonts
-cd ${2:-~/Library/Application\ Support/SWPPro/Profiles/*default}
-fc-list : file family | grep -e 'ttf' -e 'otf' | sed 's/^.*: //' | sort | uniq >fontfamilies.txt
+fc-list : file family | grep -e 'ttf' -e 'otf' | sed 's/^.*: //' | sort | uniq - "$*"
+
