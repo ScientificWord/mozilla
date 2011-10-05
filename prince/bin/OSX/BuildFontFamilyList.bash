9dd9af4b0d81b251c5766a797ba66dd200fabff7
Barry MacKichhan <barry.mackichan@mackichan.com>
2011-10-05 11:51:35 -0600

    Fixed a blunder

--- prince/bin/OSX/BuildFontFamilyList.bash
+++ prince/bin/OSX/BuildFontFamilyList.bash
@@ -1,4 +1,4 @@
 #!/bin/sh
 # build list of family names of ttf and otf fonts
-/usr/X11/fc-list : file family | /usr/bin/grep -e 'ttf' -e 'otf' | /usr/bin/sed 's/^.*: //' | /usr/bin/sort | /usr/bin/uniq - "$*"
+/usr/X11/bin/fc-list : file family | /usr/bin/grep -e 'ttf' -e 'otf' | /usr/bin/sed 's/^.*: //' | /usr/bin/sort | /usr/bin/uniq - "$*"
 
