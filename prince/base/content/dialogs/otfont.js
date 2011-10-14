d1cfaeeca86d7be869a0634c594db8337ee12d76
Barry MacKichhan <barry.mackichan@mackichan.com>
2011-10-14 17:34:50 -0600

    Several things. I got rid of a problem where the VCam buttons could come up and disappear when double clicking on a frame. Fixed up the diagram on the frame panel in those cases where the frame does not touch a margin or overhang. Fixed up the otfont dialog. Not yet finished on that. Also put the cursor in place after creating a new frame.

--- prince/base/content/dialogs/otfont.js
+++ prince/base/content/dialogs/otfont.js
@@ -1,40 +1,104 @@
 Components.utils.import("resource://app/modules/fontlist.jsm"); 
 Components.utils.import("resource://app/modules/pathutils.jsm"); 
+Components.utils.import("resource://app/modules/unitHandler.jsm");
 
 var node;
-var initial;
 var data = { ruleColor: "#000000" };
 
 function startup()
 {
   if (window.arguments && window.arguments.length > 0)
-    node = window.arguments[0];
+	{
+	  node = window.arguments[0];
+	}
+  initializeFontName(node);
+  initializeFontSize(node);
+  initializeColorData(node);
+  initializeLeading(node);
+  initializeUnits(node);
+}
+
+function initializeFontName(node)
+{
   var menuObject = { };
+	var initial;
   initializeFontFamilyList(false);
   menuObject.menulist = document.getElementById("otfontlist");
   addOTFontsToMenu(menuObject);
   if (node)
   {
     initial = node.getAttribute("fontname");
-    document.getElementById("otfontlist").value = initial;
-  }
+    if (initial) 
+		{
+			document.getElementById("otfontlist").value = initial;
+		}
+  }	
+}
+
+function initializeFontSize(node)
+{
+	var size;
+	if (node)
+	{
+		size = node.getAttribute("size");
+		if (leading)
+		{
+			document.getElementById("size").value = size;
+		}		
+	}
+}
+
+function initializeColorData(node)
+{
+	var color;
+	if (node)
+	{
+		color = node.getAttribute("color");
+		if (color)
+		{
+			setColorWell("colorWell", color);
+		}		
+	}
+}
+
+function initializeLeading(node)
+{
+	var leading;
+	if (node)
+	{
+		leading = node.getAttribute("leading");
+		if (leading)
+		{
+			document.getElementById("leading").value = leading;
+		}		
+	}
+}
+
+function initializeUnits(node)
+{
+	var unit;
+	if (node)
+	{
+		unit = node.getAttribute("units");
+		if (unit)
+		{
+			document.getElementsById("otfont.units");
+		}
+	}
 }
 
 function getColorAndUpdate()
 {
   var colorWell = document.getElementById("colorWell");
   if (!colorWell) return;
+  var color;
 
-  var colorObj = { NoDefault: false, Type: "Rule", TextColor: data.ruleColor, PageColor: 0, Cancel: false };
-
+  var colorObj = { NoDefault: false, Type: "Rule", TextColor: color, PageColor: 0, Cancel: false };
   window.openDialog("chrome://editor/content/EdColorPicker.xul", "colorpicker", "chrome,close,titlebar,modal,resizable", "", colorObj);
-
-  // User canceled the dialog
   if (colorObj.Cancel)
     return;
-
-  data.ruleColor = colorObj.TextColor;
-  setColorWell("colorWell", data.ruleColor); 
+  color = colorObj.TextColor;
+  setColorWell("colorWell", color); 
 }
 
 
@@ -42,33 +106,68 @@ function getColorAndUpdate()
 
 function onAccept()
 {
-  var list = document.getElementById("otfontlist");
-  var fontname;
-  if (list.selectedIndex  > 0)
-  {
-    fontname = list.value;
-    var editorElement = msiGetParentEditorElementForDialog(window);
-    if (!editorElement)
-    {
-      AlertWithTitle("Error", "No editor in otfont.OnAccept!");
-    }
-  }
-  if (node)
-  {
-    node.setAttribute("fontname", fontname);
-    node.setAttribute("style","font-family: "+fontname+";");
-  }
-  else
-  {
-	  var theWindow = window.opener;
-	  if (!theWindow || !("msiEditorSetTextProperty" in theWindow))
-    {
-	    theWindow = msiGetTopLevelWindow();
-    }
-    theWindow.msiRequirePackage(editorElement, "xltxtra", null);
-    theWindow.msiEditorSetTextProperty(editorElement, "otfont", "fontname", fontname);
-  }
+  var editorElement = msiGetParentEditorElementForDialog(window);
+   if (!editorElement)
+   {
+     throw("No editor in otfont.OnAccept!");
+   }
+  var fontname = getFontName();
+	var leading = getLeading();
+	var color = getColorFromColorPicker();
+	var unit = getUnits();
+	var fontsize = getFontSize();
+	alert(fontname+","+leading+","+color+","+unit+","+fontsize);
+	
+//  if (node)
+//  {
+//    node.setAttribute("fontname", fontname);
+//    node.setAttribute("style","font-family: "+fontname+";");
+//  }
+//  else
+//  {
+//	  var theWindow = window.opener;
+//	  if (!theWindow || !("msiEditorSetTextProperty" in theWindow))
+//    {
+//	    theWindow = msiGetTopLevelWindow();
+//    }
+//    theWindow.msiRequirePackage(editorElement, "xltxtra", null);
+//    theWindow.msiEditorSetTextProperty(editorElement, "otfont", "fontname", fontname);
+//  }
   editorElement.contentWindow.focus();
+}
+
+function getFontName()
+{
+  var list = document.getElementById("otfontlist");
+	if (list.selectedIndex  > 0)
+  {
+    return list.value;
+  }
+  return null;
+}
+
+function getFontSize() 
+{
+	var size = document.getElementById("otfont.fontsize").value;
+	return size;
+}
+
+function getLeading() 
+{
+	var leading = document.getElementById("leading").value;
+	return leading;
+}
+
+function getUnits() 
+{
+	var units = document.getElementById("otfont.units").value;
+	return units;
+}
+
+function getColorFromColorPicker()
+{
+	var color = getColor("colorWell");
+	return color;
 }
 
 function onCancel()
