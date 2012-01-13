fe55757e55a6d9e2f5fc8b227f874061f100cd34
Dave Bailey <daveb@mackichan.com>
2012-01-13 14:55:19 -0700

    Revised pdflatex.bash and xelatex.bash so they provide the processor name (e.g. pdflatex) as the forth parameter to pdflatex.scpt. The script for using pdflatex and xelatex differ only by the name of the processor, so the extra script was eliminated. pdflatex.scpt now causes the Terminal window to pop to the front while pdflatex is being run and then disappear when it successfully finishes. Eventually the js will supply the name of the processor when pdflatex is called, and the xelatex.bash script will be eliminated.

--- prince/TeX/bin/mac/xelatex.bash
+++ prince/TeX/bin/mac/xelatex.bash
@@ -3,5 +3,5 @@ if [ "`uname -s`" = "Darwin" ]; then
 	export PATH=/usr/texbin:$PATH
 fi
 # $1 is the target directory, $2 is the file to compile, $3 is usually "SWP", $4 is a spare.
-osascript `dirname "$0"`/xelatex.applescript $1 $2 $3 $4 
+osascript `dirname "$0"`/pdflatex.scpt $1 $2 $3 xelatex 
 
