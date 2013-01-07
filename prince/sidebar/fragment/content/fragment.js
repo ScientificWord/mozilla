7d446bafee525022b7ad1925d7c931e8e2e36130
george <george@mackichan.com>
2013-01-07 15:15:24 -0700

    Remove \n after <data>, <context>, and <info> tags and before the close tags to get rid of extra spacing that are not part of the original fragments.

--- prince/sidebar/fragment/content/fragment.js
+++ prince/sidebar/fragment/content/fragment.js
@@ -581,11 +581,11 @@ var fragObserver =
           trans.removeDataFlavor(flavour);
         }
         
-        var sFileContent = '<?xml version="1.0"?>\n<fragment>\n  <data>\n    ' + encodeURIComponent(mimetypes.kHTMLMime) +
-          '\n  </data>\n  <context>\n    ' + encodeURIComponent(mimetypes.kHTMLContext) +
-          '\n  </context>\n  <info>\n    ' + encodeURIComponent(mimetypes.kHTMLInfo) +
-          '\n  </info>\n  <description>' +  encodeURIComponent(data.description) +
-              '</description>\n</fragment>';
+        var sFileContent = '<?xml version="1.0"?>\n<fragment>\n  <data>' + encodeURIComponent(mimetypes.kHTMLMime) +
+          '</data>\n  <context>' + encodeURIComponent(mimetypes.kHTMLContext) +
+          '</context>\n  <info>' + encodeURIComponent(mimetypes.kHTMLInfo) +
+          '</info>\n  <description>' +  encodeURIComponent(data.description) +
+          '</description>\n</fragment>';
         var urlstring = urlbasestring + path + "/" + data.filename;
         var filepath = msiPathFromFileURL( msiURIFromString(urlstring));        
         if (filepath.search(/.frg/) == -1) filepath += ".frg";
@@ -693,11 +693,11 @@ function createFragmentFromClip()
         trans.removeDataFlavor(flavour);
       }
       
-      var sFileContent = '<?xml version="1.0"?>\n<fragment>\n  <data>\n    ' + encodeURIComponent(mimetypes.kHTMLMime) +
-        '\n  </data>\n  <context>\n    ' + encodeURIComponent(mimetypes.kHTMLContext) +
-        '\n  </context>\n  <info>\n    ' + encodeURIComponent(mimetypes.kHTMLInfo) +
-        '\n  </info>\n  <description>' + encodeURIComponent(data.description) +
-            '</description>\n</fragment>';
+      var sFileContent = '<?xml version="1.0"?>\n<fragment>\n  <data>' + encodeURIComponent(mimetypes.kHTMLMime) +
+        '</data>\n  <context>' + encodeURIComponent(mimetypes.kHTMLContext) +
+        '</context>\n  <info>' + encodeURIComponent(mimetypes.kHTMLInfo) +
+        '</info>\n  <description>' + encodeURIComponent(data.description) +
+        '</description>\n</fragment>';
       var urlstring = urlbasestring + path + "/" + data.filename;
       var filepath = msiPathFromFileURL( msiURIFromString(urlstring));        
       if (filepath.search(/.frg/) == -1) filepath += ".frg";
