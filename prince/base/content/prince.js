335c86a1b3423c605e85d2ac0251228c01a26fa1
Barry MacKichan <barry.mackichan@mackichan.com>
2013-03-21 12:36:30 -0600

    Bug 1928. Expanding parens (and braces, brackets, and norm) applied to text now convert to math.

--- prince/base/content/prince.js
+++ prince/base/content/prince.js
@@ -167,37 +167,42 @@ function GetFixedMath(math)
   return runFixup(GetMathAsString(math));
 }
 
-// form a single run of math and put caret on end
-function coalescemath() {
-  var editor = GetCurrentEditor();
-  var selection = editor.selection;
-  if (selection) {
-    var f = selection.focusNode;
-    var element = findmathparent(f);
-    if (!element) {
-      f = f.previousSibling;
-      element = findmathparent(f);
-      if (!element) {
-	      dump("focus not in math!\n");
-        return;
-      }
-    }
-    var last = node_before(element);
-    if (!last || last.localName != "math") {
-      dump("previous is not math!\n");
-      return;
-    }
-    var ch = element.firstChild;  // move children to previous math element
-    var nextch;
-    while (ch) {
-      nextch = ch.nextSibling;
-      last.appendChild(element.removeChild(ch));
-      ch = nextch;
-    }
-    element.parentNode.removeChild(element);  // now empty
 
-    editor.setCaretAfterElement(last_child(last));
-} }
+//The function coalescemath is defined in computeOverlay.js
+//
+// // form a single run of math and put caret on end
+// function coalescemath() {
+//   var editor = GetCurrentEditor();
+//   var selection = editor.selection;
+//   if (selection) {
+//     var f = selection.focusNode;
+//     var element = findmathparent(f);
+//     if (!element) {
+//       f = f.previousSibling;
+//       element = findmathparent(f);
+//       if (!element) {
+// 	      dump("focus not in math!\n");
+//         return;
+//       }
+//     }
+//     var last = node_before(element);
+//     if (!last || last.localName != "math") {
+//       dump("previous is not math!\n");
+//       return;
+//     }
+//     var ch = element.firstChild;  // move children to previous math element
+//     var nextch;
+//     while (ch) {
+//       nextch = ch.nextSibling;
+//       last.appendChild(element.removeChild(ch));
+//       ch = nextch;
+//     }
+//     element.parentNode.removeChild(element);  // now empty
+
+//     editor.setCaretAfterElement(last_child(last));
+// } }
+
+
 
 function findtagparent(node,tag) {
   if (!node || node.localName == tag)
