a8ccd312df760b136e4e7732347bfe7ed6baff34
Barry MacKichan <barry.mackichan@mackichan.com>
2013-04-14 17:24:14 -0600

    Backed out changeset 7e54bce5278d Hit return too fast, checked in too many files

--- prince/base/content/dialogs/msiRulesDialog.js
+++ prince/base/content/dialogs/msiRulesDialog.js
@@ -91,7 +91,7 @@ function setUpData()
     {
       data[dataComponents[ix]] = new Object();
       data[dataComponents[ix]].units = "pt";
-      data[dataComponents[ix]].size = (ix == 0 ? 0.0 : (ix == 1 ? 100.0 : 1.0 ));
+      data[dataComponents[ix]].size = 0.00;
     }
   }
   if (!("ruleColor" in data) || data.ruleColor == null)
