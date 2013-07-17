9c8b49f431e4b916e789908624b612b2baf21ee2
Barry MacKichan <barry.mackichan@mackichan.com>
2013-07-17 13:59:20 -0600

    Fixed up fragment renaming, bug 418

--- prince/base/content/dialogs/FragmentName.js
+++ prince/base/content/dialogs/FragmentName.js
@@ -1,11 +1,12 @@
 <?xml version="1.0"?>
 <!-- Copyright (c) 2005 MacKichan Software, Inc.  All Rights Reserved. -->
 
+
 function onAccept()
 {
   var data = window.arguments[0];
   data.filename = document.getElementById("fragment-name").value;
-  data.description = document.getElementById("fragment-description").value;
+  data.description = document.getElementById("fragment-description").getAttribute('value');
 }
 
 function onCancel()
@@ -17,15 +18,13 @@ function onCancel()
 function initialize()
 {
   var data = window.arguments[0];
+  if (!(data.description && data.description.length > 0))
+    data.description = document.getElementById("descriptionprompt").getAttribute('value');
   if (data.filename && data.filename.length > 0) document.getElementById("fragment-name").value = data.filename; 
   var broadcasters = document.getElementsByTagName("broadcaster");
   var j;
   for (j=0; j< broadcasters.length; j++) broadcasters[j].setAttribute("hidden","true");
   if (data.role) document.getElementById(data.role).setAttribute("hidden","false"); 
-  if (data.role=='newfrag' || data.role == 'renamefrag') 
-  {
-    document.getElementById("fragdescription").setAttribute("hidden", "false");
-    document.getElementById("fragment-description").value = data.description;
-  }
+  document.getElementById("fragment-description").value = data.description;
 }
 
