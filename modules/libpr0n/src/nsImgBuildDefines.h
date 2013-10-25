c81c3d44caaa8a028ad908a6de225e7c0db903bb
Barry MacKichan <barry.mackichan@mackichan.com>
2013-10-25 11:37:34 -0600

    This file used to be constructed during the build process. That seemed a bit silly, and it wasn't getting built on the Mac with the new build tools, so I've just added it and commented out the building rules.

---
+++ modules/libpr0n/src/nsImgBuildDefines.h
@@ -0,0 +1,8 @@
+#define IMG_BUILD_DECODER_bmp
+#define IMG_BUILD_DECODER_gif
+#define IMG_BUILD_DECODER_jpeg
+#define IMG_BUILD_DECODER_png
+#define IMG_BUILD_DECODER_xbm
+
+#define IMG_BUILD_ENCODER_jpeg
+#define IMG_BUILD_ENCODER_png
