879263fc4f922f9425124104b194240f2fdfe87f
Barry MacKichan <barry.mackichan@mackichan.com>
2013-10-29 15:34:39 -0600

    Fixing the running of bibtex and makeindex

--- prince/TeX/bin/mac/makeindex.bash
+++ prince/TeX/bin/mac/makeindex.bash
@@ -2,7 +2,8 @@
 
 # $1 is the target directory, $2 is the file to compile, $3 is usually "SWP"
 cd $1
-/usr/texbin/makeindex $3 
-touch sentinel
+export PATH=$MSITEXBIN
+makeindex main 
+echo done > sentinel
 exit 0
 
