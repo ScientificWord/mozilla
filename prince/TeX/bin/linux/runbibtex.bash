f74c7615d904df776e5969c148c975256d5ae6d5
Barry MacKichan <barry.mackichan@mackichan.com>
2013-11-04 15:54:48 -0700

    Mirrored changes made in the Mac version. Needs testing!!

--- prince/TeX/bin/linux/runbibtex.bash
+++ prince/TeX/bin/linux/runbibtex.bash
@@ -1,7 +1,20 @@
-cd $BIBTARGDIR
+source ~/.mackichan/MSITeX.bash
+# $1 is the target directory, $2 is the file to compile, $3 is usually "SWP"
+cd $1
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    "-d")
+	  BIBINPDIR="$2"
+	  shift
+	  ;;
+  esac
+  shift
+done
+
 export PATH=$MSITEXBIN
-#echo BIBINPUTS "$BIBINPUTS", BSTINPUTS "$BSTINPUTS", BIBTARGDIR "$BIBTARGDIR", MSITEXBIN "$MSITEXBIN"
-#echo bibtex "$BIBTARGFILE"
-bibtex main
+export BIBINPUTS=$BIBINPDIR:$BIBINPUTS
+echo BIBINPUTS=$BIBINPUTS > ~/log
+bibtex main 
 echo done > sentinel
 exit 0
--- prince/TeX/bin/win/runbibtex.cmd
+++ prince/TeX/bin/win/runbibtex.cmd
@@ -1,8 +1,17 @@
+@echo off
+setlocal
+:checkParams
+if "%1"=="-d" goto setInputDir
+goto doIt
+:setInputDir
+shift
+set BIBINPDIR=%1
+shift
+goto checkParams
+:doIt
 
-pushd "%1"
 set path="%MSITEXBIN%"
+set BINPUTS=%BIBINPDIR%:%BINPUTS%
 bibtex main
 echo done > sentinel
-popd
 endlocal
-:leave
