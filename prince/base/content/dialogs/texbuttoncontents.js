90821be789032f9b53c4670d751444a3aa3995f9
Barry MacKichan <barry.mackichan@mackichan.com>
2014-02-08 18:08:27 -0700

    Evidently xhtmlns was already declared.

--- prince/base/content/dialogs/texbuttoncontents.js
+++ prince/base/content/dialogs/texbuttoncontents.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2010 MacKichan Software, Inc.  All Rights Reserved.
 
-var xhtmlns  = "http://www.w3.org/1999/xhtml";
+// var xhtmlns  = "http://www.w3.org/1999/xhtml";  Seems to be declared already.
 
 var texnode;
 var editor;
