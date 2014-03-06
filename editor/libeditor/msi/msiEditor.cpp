9c8bf3613f3f3af3c24530c27df710afadd19da4
Barry MacKichan <barry.mackichan@mackichan.com>
2014-03-05 17:55:53 -0700

    More on putting auto sub stuff into structures with a fixed number of parts.

--- editor/libeditor/msi/msiEditor.cpp
+++ editor/libeditor/msi/msiEditor.cpp
@@ -3158,6 +3158,7 @@ msiEditor::CheckForAutoSubstitute(PRBool inmath)
   GetNextCharacter(originalNode, originalOffset, getter_AddRefs(node), offset, inmath, ch, lookupResult);
   if (node)  // there was success somewhere
   {
+    SetInComplexTransaction(PR_TRUE);
     if (lookupResult == msiIAutosub::STATE_SPECIAL)
 		{
 			ctx =	msiIAutosub::CONTEXT_TEXTONLY; 
@@ -3199,6 +3200,7 @@ msiEditor::CheckForAutoSubstitute(PRBool inmath)
         }
       }
     }
+    SetInComplexTransaction(PR_FALSE);
   }
   return res;
 }
