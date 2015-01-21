7feaf6052e92ec5be82675d4f39b36c86c8b47f6
Dave Bailey <daveb@mackichan.com>
2015-01-21 16:51:14 -0700

    Modifications to about dialog to put copyright date in the dialog text instead of the graphic, change the graphic for SWP, and change the credits tab so that former contributors are in gray text.

--- prince/base/content/about.js
+++ prince/base/content/about.js
@@ -4,5 +4,9 @@ function onLoad() {
   var versionField = document.getElementById("versionField");
   var brandShortNameEl = document.getElementById("brandShortName");          
   versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (' + navigator.productSub + ')';
+  var CopyRight = document.getElementById("CopyRight");
+  var currentTime = new Date();
+  var year = currentTime.getFullYear();
+  CopyRight.value = '©' + ' ' + year + '. All rights reserved.'
 }
 
