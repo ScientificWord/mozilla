7615b66d38cbdeeb94c2c6bdf4547dd142cbfb22
Barry <barry.mackichan@mackichan.com>
2015-02-14 19:15:00 -0700

    More functionality for the Activate dialog

--- prince/base/content/dialogs/activateDialog.js
+++ prince/base/content/dialogs/activateDialog.js
@@ -1,3 +1,6 @@
+#include ../productname.inc
+///
+var req;
 
 function startUp() {
 	var editorElement = msiGetActiveEditorElement();
@@ -5,3 +8,52 @@ function startUp() {
 	var hostid = editor.mAppUtils.hostid;
 	document.getElementById('computerid').setAttribute('value', hostid);
 }
+
+function accept() {
+	var transferComplete = function() {
+		alert(this.responseText);
+	};
+
+	var transferFailed = function() {
+		alert("Failed to get license");
+	};
+
+	var transferCanceled = function() {
+		 alert("Getting a license was cancelled");
+	};
+	debugger;
+	var editorElement = msiGetActiveEditorElement();
+	var editor = msiGetEditor(editorElement);
+	var hostid = editor.mAppUtils.hostid;
+	var querystring = "?sSerial="+document.getElementById('serial').value
+		+ "&sProductName=" +
+#ifdef PROD_SW
+ "Scientific+Word"
+#endif
+#ifdef PROD_SNB
+ "Scientific+Notebook"
+#endif
+#ifdef PROD_SWP
+ "Scientific+WorkPlace"
+#endif
+ 		+ "&sComputerID=" + hostid + "&sVersion=6.0&sEmail=" + document.getElementById('email').value
+ 		+ "&sPhone=" + document.getElementById('phone').value;
+
+	try {
+		const {XMLHttpRequest} = Components.classes["@mozilla.org/appshell/appShellService;1"]
+	                                     .getService(Components.interfaces.nsIAppShellService)
+	                                     .hiddenDOMWindow;
+		req = new XMLHttpRequest();
+		req.addEventListener("load", transferComplete, true);
+		req.addEventListener("error", transferFailed, true);
+		req.open('GET','http://licensing.mackichan.com/licensing.net/licensedispenser.asmx/getlicense'+querystring, false);
+	}
+	catch(e) {
+		dump( e.message);
+	}
+	return 0;
+
+
+}
+
+
