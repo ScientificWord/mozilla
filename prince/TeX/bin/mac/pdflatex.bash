f13e455f968d3f02bb0c3240dba323ff902700c3
Barry <barry.mackichan@mackichan.com>
2015-02-14 19:17:05 -0700

    Minor changes to bash files to use MSITex command file.

--- prince/TeX/bin/mac/pdflatex.bash
+++ prince/TeX/bin/mac/pdflatex.bash
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ "`uname -s`" = "Darwin" ]; then
-	export PATH=/usr/texbin:$PATH
-fi
+source ~/.mackichan/MSITeX.bash
+export PATH=$MSITEXBIN:$PATH
+
 # $1 is the target directory, $2 is the file to compile, $3 is usually "SWP", $4 is a spare.
 cmd="osascript "`dirname "$0"`"/pdflatex.scpt "$1" "$2" "$3" pdflatex"
 echo "command is: "$cmd
-osascript `dirname "$0"`/pdflatex.scpt $1 $2 $3 pdflatex 
+osascript `dirname "$0"`/pdflatex.scpt $1 $2 $3 pdflatex
 exit 0
 
