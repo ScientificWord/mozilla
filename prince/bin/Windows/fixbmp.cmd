e7c29e4d4b4faaceeb80e0c670b09fc162c73269
Barry <barry.mackichan@mackichan.com>
2015-05-15 10:39:19 -0600

    Got rid of the black bars on Windows 3D snapshots of plots.

--- prince/bin/Windows/fixbmp.cmd
+++ prince/bin/Windows/fixbmp.cmd
@@ -1,5 +1,6 @@
 
 REM echo off
-cd %~1
-%~2\convert.exe graphics\%3.bmp -crop %80x+0+0 gcache\%3.png
+setlocal enabledelayedexpansion
+cd %1
+"%~2\convert.exe" graphics\%3.bmp -crop 80%%x+0+0 gcache\%3.png
 
