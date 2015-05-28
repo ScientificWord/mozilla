21a3396853fc6c8279b302e58d25b19f75dc3e93
Builder <daveb@mackichan.com>
2015-05-27 18:16:25 -0600

    Changed  to %1 two places in epstopdf.cmd. Changed epstoepf to epstopdf in graphicConversions.ini and added graphics directory.

--- prince/utilities/win32/epstopdf.cmd
+++ prince/utilities/win32/epstopdf.cmd
@@ -2,5 +2,5 @@ rem @echo off
 setlocal enabledelayedexpansion
 pushd %2
 set path="%MSITEXBIN%";%PATH%
-repstopdf --outfile=graphics/$1.pdf graphics/$1.eps
+repstopdf --outfile=graphics/%1.pdf graphics/%1.eps
 popd
