81e13137605bdf96d9c5d690a5ee69eaabd3afbe
Barry <barry.mackichan@mackichan.com>
2015-06-11 18:23:21 -0600

    Changed the version number to 6.0.1, and the build date to 06/13/2015

--- prince/base/content/about.js
+++ prince/base/content/about.js
@@ -3,7 +3,7 @@
 function onLoad() {
   var versionField = document.getElementById("versionField");
   var brandShortNameEl = document.getElementById("brandShortName");
-  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.0 06/04/2015)';
+  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.1 06/13/2015)';
   var CopyRight = document.getElementById("CopyRight");
   var currentTime = new Date();
   var year = currentTime.getFullYear();
