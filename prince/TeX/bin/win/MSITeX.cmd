cd8e00682ca67d1f60b8fefe9a0b08bb3a8a0c11
Dave Bailey <daveb@mackichan.com>
2015-06-23 16:43:38 -0600

    Remove GS path gueses.

--- prince/TeX/bin/win/MSITeX.cmd
+++ prince/TeX/bin/win/MSITeX.cmd
@@ -3,5 +3,5 @@ setx MSITEX @@TeX_Root@@
 setx MSITEXBIN @@TeX_Bindir@@
 setx MSITEXMF @@TeXmf_Local@@
 setx MSIBIBTEX @@BibTeX_Dir@@
-setx PATH "c:\Program Files (x86)\gs\gs9.14\bin;c:\Program Files\gs\gs9.14\bin;@@TeX_Bindir@@;%PATH%"
+setx PATH "@@TeX_Bindir@@;%PATH%"
 setx BIBINPUTS %MSIBIBTEX%
