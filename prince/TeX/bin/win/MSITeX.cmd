86ac2bfa131b9095edf8d260b769b7fd0d4c3c05
Dave Bailey <daveb@mackichan.com>
2015-06-23 13:39:04 -0600

    MSITeX.cmd includes set as well as setx commands

--- prince/TeX/bin/win/MSITeX.cmd
+++ prince/TeX/bin/win/MSITeX.cmd
@@ -5,3 +5,9 @@ setx MSITEXMF @@TeXmf_Local@@
 setx MSIBIBTEX @@BibTeX_Dir@@
 setx PATH "c:\Program Files (x86)\gs\gs9.14\bin;c:\Program Files\gs\gs9.14\bin;@@TeX_Bindir@@;%PATH%"
 setx BIBINPUTS %MSIBIBTEX%;
+set MSITEX=@@TeX_Root@@
+set MSITEXBIN=@@TeX_Bindir@@
+set MSITEXMF=@@TeXmf_Local@@
+set MSIBIBTEX=@@BibTeX_Dir@@
+set PATH="c:\Program Files (x86)\gs\gs9.14\bin;c:\Program Files\gs\gs9.14\bin;@@TeX_Bindir@@;%PATH%"
+set BIBINPUTS=%MSIBIBTEX%;
