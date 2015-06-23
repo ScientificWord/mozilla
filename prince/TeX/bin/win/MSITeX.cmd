dad03815fc0146a020e0eb677a8c87583ac302b9
Dave Bailey <daveb@mackichan.com>
2015-06-23 14:26:54 -0600

    Remove set and fix typo.

--- prince/TeX/bin/win/MSITeX.cmd
+++ prince/TeX/bin/win/MSITeX.cmd
@@ -4,10 +4,4 @@ setx MSITEXBIN @@TeX_Bindir@@
 setx MSITEXMF @@TeXmf_Local@@
 setx MSIBIBTEX @@BibTeX_Dir@@
 setx PATH "c:\Program Files (x86)\gs\gs9.14\bin;c:\Program Files\gs\gs9.14\bin;@@TeX_Bindir@@;%PATH%"
-setx BIBINPUTS %MSIBIBTEX%;
-set MSITEX=@@TeX_Root@@
-set MSITEXBIN=@@TeX_Bindir@@
-set MSITEXMF=@@TeXmf_Local@@
-set MSIBIBTEX=@@BibTeX_Dir@@
-set PATH="c:\Program Files (x86)\gs\gs9.14\bin;c:\Program Files\gs\gs9.14\bin;@@TeX_Bindir@@;%PATH%"
-set BIBINPUTS=%MSIBIBTEX%;
+setx BIBINPUTS %MSIBIBTEX%
