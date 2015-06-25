11a8b466309f64e5822d6b179c0f1cfd8246772e
Dave Bailey <daveb@mackichan.com>
2015-06-25 10:46:17 -0600

    Fix up MSITeX.cmd

--- prince/TeX/bin/win/MSITeX.cmd
+++ prince/TeX/bin/win/MSITeX.cmd
@@ -1,7 +1,7 @@
 rem @echo off
-setx MSITEX @@TeX_Root@@
-setx MSITEXBIN @@TeX_Bindir@@
-setx MSITEXMF @@TeXmf_Local@@
-setx MSIBIBTEX @@BibTeX_Dir@@
+setx MSITEX "@@TeX_Root@@"
+setx MSITEXBIN "@@TeX_Bindir@@"
+setx MSITEXMF "@@TeXmf_Local@@"
+setx MSIBIBTEX "@@BibTeX_Dir@@"
 setx PATH "@@TeX_Bindir@@;%PATH%"
-setx BIBINPUTS %MSIBIBTEX%
+setx BIBINPUTS "@@BibTeX_Dir@@"
