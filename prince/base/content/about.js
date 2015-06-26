85c87870e1d6df02b568abd220eaf755d6326955
george <george@mackichan.com>
2015-06-26 13:50:00 -0600

    Release was delayed, change release date to 06/26/2015.

--- prince/base/content/about.js
+++ prince/base/content/about.js
@@ -3,7 +3,7 @@
 function onLoad() {
   var versionField = document.getElementById("versionField");
   var brandShortNameEl = document.getElementById("brandShortName");
-  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.2 06/24/2015)';
+  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.2 06/26/2015)';
   var CopyRight = document.getElementById("CopyRight");
   var currentTime = new Date();
   var year = currentTime.getFullYear();
