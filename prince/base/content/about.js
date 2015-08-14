dfe754ffaf36a1cbee047c0499f70ac5c53ad22b
george <devnull@localhost>
2015-08-13 20:54:48 -0600

    Target date for the next Wednesday release, 08/19/2015.

--- prince/base/content/about.js
+++ prince/base/content/about.js
@@ -3,7 +3,7 @@
 function onLoad() {
   var versionField = document.getElementById("versionField");
   var brandShortNameEl = document.getElementById("brandShortName");
-  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.5 testing version)';
+  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.5 08/19/2015)';
   var CopyRight = document.getElementById("CopyRight");
   var currentTime = new Date();
   var year = currentTime.getFullYear();
