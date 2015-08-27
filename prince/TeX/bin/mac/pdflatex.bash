60a35482ac46caf348f755001442caf87bb014cc
Barry MacKichan <barry.mackichan@mackichan.com>
2015-08-27 17:26:25 -0600

    Preview now works on the Mac with files with spaces in their names. I haven't verified that it hasn't screwed up anything on the Windows side, though.

--- prince/TeX/bin/mac/pdflatex.bash
+++ prince/TeX/bin/mac/pdflatex.bash
@@ -1,11 +1,14 @@
 #!/bin/sh
-
 source ~/.mackichan/MSITeX.bash
 export PATH=$MSITEXBIN:$PATH
 
+echo $1
+eval cd "'"$1"'"
+echo `pwd`
 # $1 is the target directory, $2 is the file to compile, $3 is usually "SWP", $4 is a spare.
-cmd="osascript "`dirname "$0"`"/pdflatex.scpt "$1" "$2" "$3" pdflatex"
-echo "command is: "$cmd
-osascript `dirname "$0"`/pdflatex.scpt $1 $2 $3 pdflatex
-exit 0
+# cmd="osascript "`dirname "$0"`"/pdflatex.scpt "$1" "$2" "$3" pdflatex"
+# echo "command is: "$cmd
+# osascript `dirname "$0"`/pdflatex.scpt "$1" $2 $3 pdflatex
+pdflatex -interaction=batchmode -shell-escape $2
+echo done > sentinel
 
