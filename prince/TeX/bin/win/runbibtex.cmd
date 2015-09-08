a2d116cb9cfede0e48e0f9d7a1ce03c02c174358
Barry MacKichan <barry.mackichan@mackichan.com>
2015-09-07 19:54:29 -0600

    Fixing BibTeX

--- prince/TeX/bin/win/runbibtex.cmd
+++ prince/TeX/bin/win/runbibtex.cmd
@@ -1,18 +1,29 @@
 @echo off
 setlocal enabledelayedexpansion
+# $1 is the tex directory we are working in
+# optional "-d <directory>" for a non-standard BIBINPUTS
+
+setx BIBINPUTS=
 cd %1
 shift
-:checkParams
+
+:Loop
+IF "%1"=="" GOTO Continue
 if "%1"=="-d" goto setInputDir
+SHIFT
+GOTO Loop
+:Continue
+
+
 goto doIt
 :setInputDir
 shift
-setx BIBINPUTS=%1:%BIBINPUTS%
-shift
-goto checkParams
+setx BIBINPUTS=%1
+
 :doIt
 
-set path="%MSITEXBIN%"
+setx TEXMF_HOME=%MSITEXMF_HOME%
+set path="%MSITEXBIN%":%path%
 bibtex main
 echo done > sentinel
 endlocal
