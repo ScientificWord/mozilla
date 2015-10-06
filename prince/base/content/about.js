cf4b9f60c189d909ce3aaba35eb19f508e2ad86b
george <george@mackichan.com>
2015-10-06 17:08:15 -0600

    Set date to 10/07/2015

--- prince/base/content/about.js
+++ prince/base/content/about.js
@@ -1,9 +1,9 @@
-
+﻿
 
 function onLoad() {
   var versionField = document.getElementById("versionField");
   var brandShortNameEl = document.getElementById("brandShortName");
-  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.7 10/02/2015)';
+  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.7 10/07/2015)';
   var CopyRight = document.getElementById("CopyRight");
   var currentTime = new Date();
   var year = currentTime.getFullYear();
