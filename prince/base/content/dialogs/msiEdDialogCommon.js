b280c19d97883fc96c33d7a4c0f5bf793f10cd73
Barry MacKichan <barry.mackichan@mackichan.com>
2015-10-07 11:47:49 -0600

    Got the advanced edit dialog to work in the image dialog.

--- prince/base/content/dialogs/msiEdDialogCommon.js
+++ prince/base/content/dialogs/msiEdDialogCommon.js
@@ -311,7 +311,7 @@ function onAdvancedEdit()
     window.AdvancedEditOK = false;
     // Open the AdvancedEdit dialog, passing in the element to be edited
     //  (the copy named "globalElement")
-    window.openDialog("chrome://editor/content/EdAdvancedEdit.xul", "advancededit", "chrome,close,titlebar,modal,resizable=yes", "", globalElement);
+    window.openDialog("chrome://editor/content/EdAdvancedEdit.xul", "advancededit", "chrome,close,titlebar,modal,resizable=yes", "", gGlobalElement);
     window.focus();
     if (window.AdvancedEditOK)
     {
