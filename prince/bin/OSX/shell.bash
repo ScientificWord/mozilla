91b9b33563545573a3fa008343a189f13a59cf0a
Barry MacKichan <barry.mackichan@mackichan.com>
2015-11-01 10:46:42 -0700

    Cleanup and putting a <br/> at the end of an msidisplay, which improves the behavior of the cursor in displays.

--- prince/bin/OSX/shell.bash
+++ prince/bin/OSX/shell.bash
@@ -1,12 +1,12 @@
 #! /bin/bash
 # echo "Calling open $1" > $HOME/shell.log
 # open -W $1 >> $HOME/shell.log 2>&1 &
-echo 1: $1
-echo 2: $2
-echo 3: $3
-echo 4: $4
+# echo 1: $1
+# echo 2: $2
+# echo 3: $3
+# echo 4: $4
 
 eval cd "'"$1"'"
-echo `pwd`
+# echo `pwd`
 open $2 $3 $4
 
