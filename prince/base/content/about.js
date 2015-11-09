471a85a9c59ebb0420eced6b91ccd3741540785b
george <devnull@localhost>
2015-11-08 20:32:46 -0700

    Try for Monday

--- prince/base/content/about.js
+++ prince/base/content/about.js
@@ -3,7 +3,7 @@
 function onLoad() {
   var versionField = document.getElementById("versionField");
   var brandShortNameEl = document.getElementById("brandShortName");
-  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.8 11/06/2015)';
+  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.8 11/09/2015)';
   var CopyRight = document.getElementById("CopyRight");
   var currentTime = new Date();
   var year = currentTime.getFullYear();
