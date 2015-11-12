0fc032d3ee7f37cf8ea15e567d23ba8313203e4e
george <george@mackichan.com>
2015-11-11 17:23:06 -0700

    Thursday now seems likely, plus delete some crap characters that somehow, some time, got introduced at the very beginning of the file (non-ASCII characters).

--- prince/base/content/about.js
+++ prince/base/content/about.js
@@ -1,9 +1,8 @@
-﻿
 
 function onLoad() {
   var versionField = document.getElementById("versionField");
   var brandShortNameEl = document.getElementById("brandShortName");
-  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.8 11/11/2015)';
+  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.8 11/12/2015)';
   var CopyRight = document.getElementById("CopyRight");
   var currentTime = new Date();
   var year = currentTime.getFullYear();
