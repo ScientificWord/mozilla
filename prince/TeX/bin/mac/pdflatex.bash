89ec56bda51039c275a63f2962406bb9885f2f4b
Dave Bailey <daveb@mackichan.com>
2016-02-03 18:20:15 -0700

    Instrumented version of pdflatex.bash for Mac. Creates ~/msi.log file when run

--- prince/TeX/bin/mac/pdflatex.bash
+++ prince/TeX/bin/mac/pdflatex.bash
@@ -5,10 +5,25 @@ then
 else
 	source /Applications/MacKichan/MSITeX.bash
 fi
+
+date > ~/msi.log
+echo MacKichan Software PDF compile log
 export PATH=$MSITEXBIN:$PATH
-
-eval cd "'"$1"'"
+echo PATH: $PATH >> ~/msi.log
+cd $1
+echo pdflatex location: >> ~/msi.log
+which pdflatex >> ~/msi.log
+echo Current working directory: >> ~/msi.log
+pwd >> ~/msi.log
+echo pdflatex.bash parameters: >> ~/msi.log
+echo "'"$1"'" >> ~/msi.log
+echo "'"$2"'" >> ~/msi.log
+echo "'"$3"'" >> ~/msi.log
+echo "'"$4"'" >> ~/msi.log
+echo pdflatex output: >> ~/msi.log
 # $1 is the target directory, $2 is the file to compile, $3 is usually "SWP", $4 is a spare.
-pdflatex -interaction=batchmode -shell-escape $2
+pdflatex -interaction=batchmode -shell-escape $2 >> ~/msi.log 2>&1
 echo done > sentinel
-
+echo Working directory files: >> ~/msi.log
+ls -l $1 >> ~/msi.log
+echo done >> ~/msi.log
