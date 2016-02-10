a8f3e8744f826130d362bfe4c0385e8563a06774
Barry MacKichan <barry.mackichan@mackichan.com>
2016-02-10 12:48:50 -0700

    Changed Windows cmd files to import msitex.cmd via the MSIPATHS environment variable, and to pause command windows when SWPDEBUG is defined in the environment.

--- prince/TeX/bin/win/pdflatex.cmd
+++ prince/TeX/bin/win/pdflatex.cmd
@@ -1,7 +1,9 @@
 rem @echo off
+call %MSIPATHS%
 setlocal enabledelayedexpansion
 pushd %1
 set path="%MSITEXBIN%";%path%
 pdflatex -interaction=batchmode -shell-escape %2
 echo done > sentinel
 popd
+if defined %SWPDEBUG% pause
