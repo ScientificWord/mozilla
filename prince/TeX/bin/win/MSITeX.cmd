87a0e820bd39e65308fd5b52d497bdf12bf86978
Barry MacKichan <barry.mackichan@mackichan.com>
2016-02-10 17:24:59 -0700

    Fixing typos

--- prince/TeX/bin/win/MSITeX.cmd
+++ prince/TeX/bin/win/MSITeX.cmd
@@ -2,6 +2,6 @@ rem @echo off
 set MSITEX=@@TeX_Root@@
 set MSITEXBIN=@@TeX_Bindir@@
 set MSITEXMF=@@TeXmf_Local@@
-set MSITEXMF_HOME=%MSITEX\texmf-dist
+set MSITEXMF_HOME=%MSITEX%\texmf-dist
 set PATH=@@TeX_Bindir@@;%PATH%
 set PATH=C:\Program Files (x86)\gs\gs9.16\bin;%PATH%
