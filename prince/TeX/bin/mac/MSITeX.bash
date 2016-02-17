224f207092be7435ec87522a4579664fc3ebe5ac
Barry MacKichan <barry.mackichan@mackichan.com>
2016-02-17 09:43:17 -0700

    Put /usr/local/bin in the path where it belongs

--- prince/TeX/bin/mac/MSITeX.bash
+++ prince/TeX/bin/mac/MSITeX.bash
@@ -2,5 +2,5 @@ export MSITEX=/usr/local/texlive/2015
 export MSITEXBIN=/Library/TeX/texbin
 export MSITEXMF=/usr/local/texlive/texmf-local
 export MSITEXMF_HOME=$MSITEX/texmf-dist
-export PATH=$MSITEXBIN:$PATH
+export PATH=$MSITEXBIN:/usr/local/bin:$PATH
 export TEXMFLOCAL=$MSITEXMF
