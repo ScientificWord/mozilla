79a39a965a18c8e5bbf77ae80dca664395ea165e
Barry MacKichan <barry.mackichan@mackichan.com>
2016-04-13 09:57:28 -0600

    Bug 3497. Compute in place in numerator no longer deletes the numerator.

--- prince/base/content/computeOverlay.js
+++ prince/base/content/computeOverlay.js
@@ -1363,12 +1363,14 @@ function doEvalComputation(mathElement, op, joiner, remark, editorElement, inPla
       sel.collapseToStart();
       var destNode = sel.anchorNode;
       var destOffset = sel.anchorOffset;
-      if (!msiNavigationUtils.isMathNode(sel.anchorNode)) {
+      if (!(msiNavigationUtils.isMathNode(sel.anchorNode)||msiNavigationUtils.isMathNode(sel.anchorNode.parentNode))) {
         destNode = editor.document.createElementNS("http://www.w3.org/1998/Math/MathML", "math");
         editor.insertNode(destNode, sel.anchorNode, sel.anchorOffset);
         destOffset = 0;
       }
       insertXML(editor, out, destNode, destOffset);
+      if (destNode.nodeName==='mi') destNode.removeAttribute("tempinput");
+      if (destNode.parentNode.nodeName==='mi') destNode.parentNode.removeAttribute("tempinput");
     }
     coalescemath(editorElement);
   } catch (e) {
