2d9792a0f6fb235ac3c895ebcd2aa1a345c304ec
george <george@mackichan.com>
2016-05-03 10:18:11 -0600

    Next up, 6.0.15 with first guess release date three Wednesday's away.

--- prince/base/content/about.js
+++ prince/base/content/about.js
@@ -2,7 +2,7 @@
 function onLoad() {
   var versionField = document.getElementById("versionField");
   var brandShortNameEl = document.getElementById("brandShortName");
-  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.14 04/29/2016)';
+  versionField.value = brandShortNameEl.firstChild.nodeValue + ' ' + navigator.vendorSub + ' (6.0.15 05/25/2016)';
   var CopyRight = document.getElementById("CopyRight");
   var currentTime = new Date();
   var year = currentTime.getFullYear();
