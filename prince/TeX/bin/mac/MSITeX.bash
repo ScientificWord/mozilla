4c58b5d7ca1f9f0c8546fc0c57e4e5e62000e676
Dave Bailey <daveb@mackichan.com>
2016-06-21 15:12:42 -0600

    Use TeX Live year instead of wired dates (e.g. 2015)

--- prince/TeX/bin/mac/MSITeX.bash
+++ prince/TeX/bin/mac/MSITeX.bash
@@ -1,4 +1,17 @@
-export MSITEX=/usr/local/texlive/2015
+#! /bin/bash
+if [ -e ~/.tex/tlyear ]
+	then
+	tlyear=`cat ~/.tex/tlyear`
+else
+	if [ -e /usr/local/texlive/tlyear ]
+		then
+		tlyear=`cat /usr/local/texlive/tlyear`
+	else
+		tlyear='2015'
+	fi
+fi
+
+export MSITEX=/usr/local/texlive/$tlyear
 export MSITEXBIN=/Library/TeX/texbin
 export MSITEXMF=/usr/local/texlive/texmf-local
 export MSITEXMF_HOME=$MSITEX/texmf-dist
--- prince/TeX/bin/win/MSITeX.cmd
+++ prince/TeX/bin/win/MSITeX.cmd
@@ -1,9 +1,14 @@
 rem @echo off
-set MSITEX=@@TeX_Root@@
-set MSITEXBIN=@@TeX_Bindir@@
-set MSITEXMF=@@TeXmf_Local@@
+if EXIST %USERPROFILE%\.tex\tlyear (
+     set /P tlyear=<%USERPROFILE%\.tex\tlyear
+) ELSE (
+     set /P tlyear=<c:\texlive\tlyear
+)
+set MSITEX=C:\texlive\%tlyear%
+set MSITEXBIN=C:\texlive\%tlyear%\bin\win32
+set MSITEXMF=C:\texlive\texmf-local
 set MSITEXMF_HOME=%MSITEX%\texmf-dist
-set PATH=@@TeX_Bindir@@;@@GSPATH@@;%PATH%
+set PATH=C:\texlive\%tlyear%\bin\win32;C:\Program Files (x86)\gs\gs9.16\bin;%PATH%
 set TEXMFLOCAL=%MSITEXMF%
 if EXIST %programfiles(x86)%\Inkscape (
 	 set INKSCAPE=%programfiles(x86)%\Inkscape\inkscape.com
