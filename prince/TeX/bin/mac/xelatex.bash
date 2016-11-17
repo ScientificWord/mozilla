10014e5f1ca34fe32d5963b66a47d6a409675945
Dave Bailey <daveb@mackichan.com>
2016-11-17 14:56:41 -0700

    Changed xelatex to eliminate use of Applescript.

--- prince/TeX/bin/mac/xelatex.bash
+++ prince/TeX/bin/mac/xelatex.bash
@@ -1,6 +1,23 @@
 #!/bin/sh
-
 source "$1"
-# $1 is the path of the MSITeX.bash file, $2 is the target directory, $3 is the file to compile, $4 is usually "SWP", $5 is a spare.
-osascript `dirname "$0"`/pdflatex.scpt $2 $3 $4 xelatex 
-
+date > ~/msi.log
+echo MacKichan Software xelatex compile log >> ~/msi.log
+export PATH=$MSITEXBIN:$PATH
+echo PATH: $PATH >> ~/msi.log
+cd "$2"
+echo xelatex location: >> ~/msi.log
+which xelatex >> ~/msi.log
+echo Current working directory: >> ~/msi.log
+pwd >> ~/msi.log
+echo xelatex.bash parameters: >> ~/msi.log
+echo "'"$1"'" >> ~/msi.log
+echo "'"$2"'" >> ~/msi.log
+echo "'"$3"'" >> ~/msi.log
+echo "'"$4"'" >> ~/msi.log
+echo xelatex output: >> ~/msi.log
+# $1 is location of MSITeX.bash, $2 is the target directory, $3 is the file to compile, $4 is usually "SWP", $5 is a spare.
+xelatex -interaction=batchmode -shell-escape $3 >> ~/msi.log 2>&1
+echo done > sentinel
+echo Working directory files: >> ~/msi.log
+ls -l  >> ~/msi.log
+echo done >> ~/msi.log
