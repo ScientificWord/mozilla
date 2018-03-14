a351b80b029cd7f97b9f465995a39c234675cb12
george <george@mackichan.com>
2018-03-14 14:16:36 -0600

    Bug 3968

--- prince/bin/Windows/shell.cmd
+++ prince/bin/Windows/shell.cmd
@@ -1,8 +1,9 @@
-
 echo off
+if %MSIPATHS%x==x goto skip
 call "%MSIPATHS%"
+:skip
 set T=%MSITEXBIN%
-if %T%x==x set T=c:\texlive\2014msi\bin\win32
+if %T%x==x set T=c:\texlive\2017\bin\win32
 path=%T%;%windir%\system32
 cd %1
 %2 %3 %4 %5 %6
