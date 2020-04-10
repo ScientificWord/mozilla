ce262500ecf433fe5280cd382b53a3a448ed1d7f
george <george@mackichan.com>
2020-04-09 22:57:46 -0600

    Use same command line options as when running pdflatex.

--- prince/TeX/bin/win/xelatex.cmd
+++ prince/TeX/bin/win/xelatex.cmd
@@ -2,8 +2,8 @@ rem @echo off
 call %1
 setlocal enabledelayedexpansion
 pushd "%2"
-set path="%MSITEXBIN%"
-xelatex "%3"
+set path="%MSITEXBIN%";%path%
+xelatex -interaction=batchmode -shell-escape %3
 echo done > sentinel
 popd
 if defined SWPDEBUG pause
