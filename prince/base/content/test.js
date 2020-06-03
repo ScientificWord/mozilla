cc7c6754fc7e331e2e949cf5c4ceaca52a84bee2
Barry <barry.mackichan@mackichn.com>
2020-06-03 11:13:03 -0400

    Created files to test the previous filess

---
+++ prince/base/content/test.js
@@ -0,0 +1,20 @@
+
+
+var theStringSource = "starting string";
+
+function onLoad() {
+	try {
+	  var editorElement = document.getElementById("testEdit");
+	  var editorInitializer = new msiEditorArrayInitializer();
+	  editorInitializer.addEditorInfo(editorElement, theStringSource, true, true);
+	  editorInitializer.doInitialize();
+		msiSetInitialDialogFocus(editorElement);
+		msiSetActiveEditor(editorElement, false);
+		msiGetEditor(editorElement).SetTopXULWindow(window);
+		msiGetEditor(editorElement).selectAll();
+	}
+	catch(e) {
+		dump('Error: ' + e.message);
+	}
+  
+}
