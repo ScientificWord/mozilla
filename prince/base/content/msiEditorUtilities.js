21f8974d01b7275ca3651a198deb0cebb4636a64
Barry <barry.mackichan@mackichn.com>
2020-07-24 16:41:04 -0400

    Changes to code that had been generating exceptions. The plot and plotlabels dialogs are working much better for me.

--- prince/base/content/msiEditorUtilities.js
+++ prince/base/content/msiEditorUtilities.js
@@ -19,68 +19,68 @@ var gProcessor; //xslt processor
 /*
 JavaScript enhancements
 */
-if (!String.fromCodePoint) {
-  (function() {
-    var defineProperty = (function() {
-      var result;
-      // IE 8 only supports `Object.defineProperty` on DOM elements
-      try {
-        var object = {};
-        var $defineProperty = Object.defineProperty;
-        result = $defineProperty(object, object, object) && $defineProperty;
-      } catch(error) {}
-      return result;
-    }());
-    var stringFromCharCode = String.fromCharCode;
-    var floor = Math.floor;
-    var fromCodePoint = function() {
-      var MAX_SIZE = 0x4000;
-      var codeUnits = [];
-      var highSurrogate;
-      var lowSurrogate;
-      var index = -1;
-      var length = arguments.length;
-      if (!length) {
-        return '';
-      }
-      var result = '';
-      while (++index < length) {
-        var codePoint = Number(arguments[index]);
-        if (
-          !isFinite(codePoint) ||       // `NaN`, `+Infinity`, or `-Infinity`
-          codePoint < 0 ||              // not a valid Unicode code point
-          codePoint > 0x10FFFF ||       // not a valid Unicode code point
-          floor(codePoint) != codePoint // not an integer
-        ) {
-          throw RangeError('Invalid code point: ' + codePoint);
-        }
-        if (codePoint <= 0xFFFF) { // BMP code point
-          codeUnits.push(codePoint);
-        } else { // Astral code point; split in surrogate halves
-          // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-          codePoint -= 0x10000;
-          highSurrogate = (codePoint >> 10) + 0xD800;
-          lowSurrogate = (codePoint % 0x400) + 0xDC00;
-          codeUnits.push(highSurrogate, lowSurrogate);
-        }
-        if (index + 1 == length || codeUnits.length > MAX_SIZE) {
-          result += stringFromCharCode.apply(null, codeUnits);
-          codeUnits.length = 0;
-        }
-      }
-      return result;
-    };
-    if (defineProperty) {
-      defineProperty(String, 'fromCodePoint', {
-        'value': fromCodePoint,
-        'configurable': true,
-        'writable': true
-      });
-    } else {
-      String.fromCodePoint = fromCodePoint;
-    }
-  }());
-}
+// if (!String.fromCodePoint) {
+//   (function() {
+//     var defineProperty = (function() {
+//       var result;
+//       // IE 8 only supports `Object.defineProperty` on DOM elements
+//       try {
+//         var object = {};
+//         var $defineProperty = Object.defineProperty;
+//         result = $defineProperty(object, object, object) && $defineProperty;
+//       } catch(error) {}
+//       return result;
+//     }());
+//     var stringFromCharCode = String.fromCharCode;
+//     var floor = Math.floor;
+//     var fromCodePoint = function() {
+//       var MAX_SIZE = 0x4000;
+//       var codeUnits = [];
+//       var highSurrogate;
+//       var lowSurrogate;
+//       var index = -1;
+//       var length = arguments.length;
+//       if (!length) {
+//         return '';
+//       }
+//       var result = '';
+//       while (++index < length) {
+//         var codePoint = Number(arguments[index]);
+//         if (
+//           !isFinite(codePoint) ||       // `NaN`, `+Infinity`, or `-Infinity`
+//           codePoint < 0 ||              // not a valid Unicode code point
+//           codePoint > 0x10FFFF ||       // not a valid Unicode code point
+//           floor(codePoint) != codePoint // not an integer
+//         ) {
+//           throw RangeError('Invalid code point: ' + codePoint);
+//         }
+//         if (codePoint <= 0xFFFF) { // BMP code point
+//           codeUnits.push(codePoint);
+//         } else { // Astral code point; split in surrogate halves
+//           // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+//           codePoint -= 0x10000;
+//           highSurrogate = (codePoint >> 10) + 0xD800;
+//           lowSurrogate = (codePoint % 0x400) + 0xDC00;
+//           codeUnits.push(highSurrogate, lowSurrogate);
+//         }
+//         if (index + 1 == length || codeUnits.length > MAX_SIZE) {
+//           result += stringFromCharCode.apply(null, codeUnits);
+//           codeUnits.length = 0;
+//         }
+//       }
+//       return result;
+//     };
+//     if (defineProperty) {
+//       defineProperty(String, 'fromCodePoint', {
+//         'value': fromCodePoint,
+//         'configurable': true,
+//         'writable': true
+//       });
+//     } else {
+//       String.fromCodePoint = fromCodePoint;
+//     }
+//   }());
+// }
 
 /*
 Until we sync with Mozilla again, we need to define our own Object.create and
@@ -4181,25 +4181,35 @@ function msiMakeUrlRelativeTo(inputUrl, baseUrl, editorElement) {
 function msiMakeAbsoluteUrl(url, editorElement) {
   if (!editorElement)
     editorElement = msiGetActiveEditorElement();
+  var docUrl;
+  var docUri;
+  var docScheme;
+  var IOService;
   var resultUrl = TrimString(url);
-  if (!resultUrl)
-    return resultUrl;
   // Check if URL is already absolute, i.e., it has a scheme
-  var urlScheme = GetScheme(resultUrl);
-  if (urlScheme)
-    return resultUrl;
-  var docUrl = msiGetDocumentBaseUrl(editorElement);
-  var docScheme = GetScheme(docUrl);
+  var urlScheme;
+  try {
+    urlScheme = GetScheme(resultUrl);
+    if (urlScheme)
+      return resultUrl;
+  }
+  catch(e) {}
   // Can't relativize if no doc scheme (page hasn't been saved)
-  if (!docScheme)
+  try {
+    docUrl = msiGetDocumentBaseUrl(editorElement);
+    docScheme = GetScheme(docUrl);
+    IOService = msiGetIOService();
+  }
+  catch(e) {
     return resultUrl;
-  var IOService = msiGetIOService();
-  if (!IOService)
+  }
+
+  if (!docScheme || !IOService)
     return resultUrl;
   // Make a URI object to use its "resolve" method
   var absoluteUrl = resultUrl;
-  var docUri = IOService.newURI(docUrl, msiGetCurrentEditor().documentCharacterSet, null);
   try {
+    docUri = IOService.newURI(docUrl, msiGetCurrentEditor().documentCharacterSet, null);
     absoluteUrl = docUri.resolve(resultUrl);  // This is deprecated and buggy!
                                               // If used, we must make it a path for the parent directory (remove filename)
                                               //absoluteUrl = IOService.resolveRelativePath(resultUrl, docUrl);
@@ -6585,8 +6595,10 @@ var msiSearchStringManager = {
     //Here as below, "aControl" is most likely a dialog window, but...
     {
       var topEditorElement = msiGetTopLevelEditorElement(aControl);
-      var topEditor = msiGetEditor(topEditorElement);
+      var topEditor;
+      if (topEditorElement) topEditor = msiGetEditor(topEditorElement);
       var aSubIdent = { mControl: aControl };
+      if (!topEditor) return '';
       return this.getSearchStringArrayRecordImp(topEditor.document, aSubIdent);
     },
   getSearchStringIDByName: function (aDocument, aName) {
@@ -9624,7 +9636,7 @@ var msiSpaceUtils = {
     if (spaceName in this.hSpaceInfo && 'charContent' in this.hSpaceInfo[spaceName])
       theContent = this.hSpaceInfo[spaceName].charContent;
     else if (spaceName in this.hSpaceInfo && 'charCode' in this.hSpaceInfo[spaceName])
-      theContent = String.fromCodePoint(this.hSpaceInfo[spaceName].charCode);
+      theContent = String.fromCharCode(this.hSpaceInfo[spaceName].charCode);
     return theContent;
   },
   getHSpaceDisplayableContent: function (spaceName) {
