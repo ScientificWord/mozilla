66fb79edd152e9618bf66881f0909a2657a5d179
Barry <barry.mackichan@mackichan.com>
2020-09-24 12:24:18 -0400

    Made the activation dialog localizable by putting strings in the dtd file

--- prince/base/content/dialogs/activateDialog.js
+++ prince/base/content/dialogs/activateDialog.js
@@ -14,21 +14,10 @@ function startUp() {
 }
 
 function accept() {
-	var acceptMessage = "Your license has been installed. Thank you for supporting ";
+	var acceptMessage = document.getElementById('thankyou').getAttribute('value');
 	var prodnum = 0;
-	acceptMessage = acceptMessage +
-#ifdef PROD_SW
-	"Scientific Word.";
-	prodnum = 2;
-#endif
-#ifdef PROD_SNB
-	"Scientific Notebook.";
-	prodnum = 1;
-#endif
-#ifdef PROD_SWP
-	"Scientific WorkPlace.";
-	prodnum = 3;
-#endif
+	acceptMessage = acceptMessage + document.getElementById('productname').getAttribute('value') + '.';
+
 
 
 	var transferComplete = function() {
@@ -37,43 +26,33 @@ function accept() {
 			if (/your site license administrator/.test(this.responseText)) {
 				match = /Your\slicense.*the site\./.exec(this.responseText);
 				if (match && match.length >= 1) {
-				  AlertWithTitle("Home Use License", match[0]);
+				  AlertWithTitle(document.getElementById('homeuse').getAttribute('value'), match[0]);
 				  return;
 				}
-
 			}
-			AlertWithTitle("Activation Failure","Failed to get license: "+this.responseText);
+			AlertWithTitle(document.getElementById('failureheader').getAttribute('value'),document.getElementById('failurelicense').getAttribute('value')+this.responseText);
 		}
 		else {
 			writeLicense(this.responseText);
-			AlertWithTitle("Activation Success", acceptMessage);
+			AlertWithTitle(document.getElementById('successtitle').getAttribute('value'), acceptMessage);
 			editor.mAppUtils.reset();
 			editor.mAppUtils.licensedApp(prodnum);
 		}
 	};
 
 	var transferFailed = function() {
-		AlertWithTitle("Activation Failure","Unable to connect to licensing server");
+		AlertWithTitle(document.getElementById('failureheader').getAttribute('value'),document.getElementById('failureserver').getAttribute('value'));
 	};
 
 	var transferCanceled = function() {
-		 AlertWithTitle("Activation Failure","Getting a license was cancelled");
+		 AlertWithTitle(document.getElementById('failureheader'),document.getElementById('failurecancelled').getAttribute('value'));
 	};
 	var editorElement = msiGetActiveEditorElement();
 	var editor = msiGetEditor(editorElement);
 	var hostid = editor.mAppUtils.hostid;
 	var url = 'http://' + server + '/licensing.net/licensedispenser.asmx/getlicense';
 	var query = { sSerial: document.getElementById('serial').value,
-		sProductName:
-#ifdef PROD_SW
- "Scientific Word",
-#endif
-#ifdef PROD_SNB
- "Scientific Notebook",
-#endif
-#ifdef PROD_SWP
- "Scientific WorkPlace",
-#endif
+		sProductName: document.getElementById('productname').value,
  		sComputerID: hostid,
  		sVersion: "6.0",
  		sEmail: document.getElementById('email').value,
@@ -103,7 +82,7 @@ function accept() {
 		req.send(querystring);
 	}
 	catch(e) {
-		dump( e.message);
+		finalT;
 	}
 	return true;
 }
