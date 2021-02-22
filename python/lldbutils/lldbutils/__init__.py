14d5d63fae253e1d25b9a3271bfaa359df751583
Barry <barry.mackichan@mackichan.com>
2021-02-22 11:51:28 -0500

    Mac-only changes to XCode debugger Python code

--- python/lldbutils/lldbutils/__init__.py
+++ python/lldbutils/lldbutils/__init__.py
@@ -1,6 +1,6 @@
 import lldb
 
-__all__ = ['content', 'lldb_ksdiff', 'general', 'gfx', 'layout', 'utils']
+__all__ = ['content', 'general', 'gfx', 'layout', 'utils']
 
 def init():
     for name in __all__:
