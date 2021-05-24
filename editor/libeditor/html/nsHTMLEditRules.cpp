a5b230ea08565dc3beaa2f47fd6746484024acd1
Barry <barry.mackichan@mackichan.com>
2021-05-24 12:44:10 -0400

    Fixed bug 4662

--- editor/libeditor/html/nsHTMLEditRules.cpp
+++ editor/libeditor/html/nsHTMLEditRules.cpp
@@ -3707,7 +3707,8 @@ PRBool IsSpecialMath(nsCOMPtr<nsIDOMElement>& node, PRBool isEmpty, PRUint32& no
   if (isMath) {
     node->GetTagName(name);
     while (node && (name.EqualsLiteral("mi") || name.EqualsLiteral("mo") || name.EqualsLiteral("mn") ||
-        name.EqualsLiteral("msqrt") || (name.EqualsLiteral("mrow") || name.EqualsLiteral("mstyle")) && empty)) {
+        // name.EqualsLiteral("msqrt") || 
+        (name.EqualsLiteral("mrow") || name.EqualsLiteral("mstyle")) && empty)) {
       editor->GetNodeLocation(node, &parent, &offset);
       node2 = do_QueryInterface(parent);
       if (empty) {
